8daeebc64a2eb39109625bd97449c5b0e76dcfbc
USAMI Kosuke <usami-k@yc5.so-net.ne.jp>
2011-01-28 08:32:52 +0900

    add Help Document menu for bundled documents

--- constants.h
+++ constants.h
@@ -260,6 +260,25 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define k_utilityMenuTag    600
 #define k_scriptMenuTag     800
 
+// Help Document Menu tag and path
+static struct {
+    int tag;
+    NSString *path;
+} k_bundleDocumentList[] = {
+    {100, @"/OgreKit License.txt"},
+    {200, @"/EN/ReadMe-en.rtf"},
+    {201, @"/EN/History-en.rtf"},
+    {202, @"/EN/Known_problems-en.rtf"},
+    {203, @"/EN/GPL.txt"},
+    {300, @"/JP/ReadMe-jp.rtf"},
+    {301, @"/JP/History-jp.rtf"},
+    {302, @"/JP/Known_problems-jp.rtf"},
+    {303, @"/JP/To_ATOK_Users-jp.rtf"},
+    {304, @"/JP/To_0.7.x_Users-jp.rtf"},
+    {305, @"/JP/GPL-euc-JP.txt"},
+    {0, nil}
+};
+
 // Goto window object index
 #define k_gotoCharacterIndex    0
 #define k_gotoLineIndex         1
