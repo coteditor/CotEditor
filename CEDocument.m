234bb5b91ff94823847861db41b5996616f446a2
USAMI Kosuke <usami.kosuke@gmail.com>
2011-10-07 08:22:08 +0900

    add autosave

--- CEDocument.m
+++ CEDocument.m
@@ -79,6 +79,17 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 @implementation CEDocument
 
+#pragma mark ===== Class method =====
+
+// ------------------------------------------------------
++ (BOOL)autosavesInPlace
+// オートセーブ/バージョン対応
+// ------------------------------------------------------
+{
+    return YES;
+}
+
+
 #pragma mark ===== Public method =====
 
 //=======================================================
