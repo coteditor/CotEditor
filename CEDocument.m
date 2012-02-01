faa8d74f2f10f7e3cf83956d0cf2fc2d50f4ad5a
USAMI Kosuke <usami.kosuke@gmail.com>
2012-02-02 08:30:12 +0900

    revert autosave feature

--- CEDocument.m
+++ CEDocument.m
@@ -86,7 +86,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 // OS X 10.7 AutoSave
 // ------------------------------------------------------
 {
-    return YES;
+    return NO;
 }
 
 
