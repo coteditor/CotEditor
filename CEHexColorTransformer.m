b3cc442c18381594a8a190cf5ddbdff0ce42aa90
1024jp <1024jp@wolfrosch.com>
2014-02-17 14:35:44 +0100

    Fix to return class object on valueTransformer

--- CEHexColorTransformer.m
+++ CEHexColorTransformer.m
@@ -47,7 +47,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 // 変換後のオブジェクトのクラスを返す
 // ------------------------------------------------------
 {
-    return [NSString self];
+    return [NSString class];
 }
 
 
