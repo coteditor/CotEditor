bc74c9f5a171e5c801c88abe1d46d428e560b238
1024jp <1024jp@wolfrosch.com>
2014-03-14 21:12:06 +0100

    Refactor CEScriptManager class

--- CEScriptManager.h
+++ CEScriptManager.h
@@ -32,18 +32,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 #import <Cocoa/Cocoa.h>
-#import <OgreKit/OgreKit.h>
-#import "constants.h"
 
 
 @interface CEScriptManager : NSObject
-{
-    IBOutlet id _errorTextView;
-
-    NSFileHandle *_outputHandle;
-    NSFileHandle *_errorHandle;
-    NSInteger _outputType;
-}
 
 // class method
 + (CEScriptManager *)sharedInstance;
