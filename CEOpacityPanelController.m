2412ad63c43b41397a884e906aa3da4978772ee8
1024jp <1024jp@wolfrosch.com>
2014-03-15 21:08:40 +0100

    Create new opacityPanelController

---
+++ CEOpacityPanelController.m
@@ -0,0 +1,98 @@
+/*
+ =================================================
+ CEOpacityPanelController
+ (for CotEditor)
+ 
+ Copyright (C) 2014 CotEditor Project
+ http://coteditor.github.io
+ =================================================
+ 
+ encoding="UTF-8"
+ Created:2014-03-12 by 1024jp
+ 
+ -------------------------------------------------
+ 
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ 
+ 
+ =================================================
+ */
+
+#import "CEOpacityPanelController.h"
+#import "CEWindowController.h"
+#import "constants.h"
+
+
+@interface CEOpacityPanelController ()
+
+@end
+
+
+@implementation CEOpacityPanelController
+
+#pragma mark Class Methods
+
+// ------------------------------------------------------
++ (CEOpacityPanelController *)sharedController
+// return shared instance
+// ------------------------------------------------------
+{
+    static dispatch_once_t predicate;
+    static CEOpacityPanelController *shared = nil;
+    
+    dispatch_once(&predicate, ^{
+        shared = [[CEOpacityPanelController alloc] initWithWindowNibName:@"OpacityPanel"];
+    });
+    
+    return shared;
+}
+
+
+
+#pragma mark Public Methods
+
+// ------------------------------------------------------
+- (instancetype)initWithWindow:(NSWindow *)window
+// default initializer
+// ------------------------------------------------------
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        id defaults = [[NSUserDefaultsController sharedUserDefaultsController] values];
+        [self setOpacity:(CGFloat)[[defaults valueForKey:k_key_windowAlpha] doubleValue]];
+    }
+    return self;
+}
+
+
+
+#pragma mark Action Messages
+
+// ------------------------------------------------------
+- (IBAction)applyAsDefault:(id)sender
+// set current value as default and apply it to all document windows
+// ------------------------------------------------------
+{
+    // apply to all windows
+    NSArray *documents = [[NSDocumentController sharedDocumentController] documents];
+    for (id document in documents) {
+        [(CEWindowController *)[document windowController] setAlpha:[self opacity]];
+    }
+    
+    // set as default
+    [[NSUserDefaultsController sharedUserDefaultsController] setObject:@([self opacity]) forKey:k_key_windowAlpha];
+}
+
+@end
