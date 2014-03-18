6368aa94308bf92d34f4d34066c359ac2f503670
1024jp <1024jp@wolfrosch.com>
2014-03-18 17:04:33 +0100

    Revert "Font preference is now applied immediately"
    This reverts commit a632dce398d48d04ce765344b1433d7bf76a3184.

--- CEPreferences.m
+++ CEPreferences.m
@@ -239,10 +239,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
         [theDefaults setFloat:theSize forKey:k_key_printFontSize];
         [self setFontFamilyNameAndSize];
     }
-    
-    for (CEDocument *document in [[NSDocumentController sharedDocumentController] documents]) {
-        [document setFontToViewInWindow];
-    }
 }
 
 
