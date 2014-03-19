c5b5946882bafe5364b8e08f56e632e73e3ef366
1024jp <1024jp@wolfrosch.com>
2014-03-19 21:05:00 +0100

    Refactor CEPrefEngodingDataSource class

--- CEPrefEncodingDataSource.h
+++ CEPrefEncodingDataSource.h
@@ -3,8 +3,9 @@
 CEPrefEncodingDataSource
 (for CotEditor)
 
-Copyright (C) 2004-2007 nakamuxu.
-http://www.aynimac.com/
+ Copyright (C) 2004-2007 nakamuxu. 
+ Copyright (C) 2014 CotEditor Project
+ http://coteditor.github.io
 =================================================
 
 encoding="UTF-8"
@@ -33,17 +34,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 #import <Cocoa/Cocoa.h>
-#import "constants.h"
+
 
 @interface CEPrefEncodingDataSource : NSObject
-{
-    IBOutlet id _tableView;
-    IBOutlet id _delSeparatorButton;
-    IBOutlet id _revertButton;
-
-    NSMutableArray *_encodingsForTmp;
-    NSArray *_draggedItems;
-}
 
 // Public method
 - (void)setupEncodingsToEdit;
@@ -53,6 +46,5 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 - (IBAction)revertDefaultEncodings:(id)sender;
 - (IBAction)addSeparator:(id)sender;
 - (IBAction)deleteSeparator:(id)sender;
-
 
 @end
