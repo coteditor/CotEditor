5f697cfee47f0dadc2cc254c4b2291659862a6d4
1024jp <1024jp@wolfrosch.com>
2014-04-23 21:31:31 +0200

    Revert "Remove a unneeded text attribute for typing text (ref #109)" (fix #112)
    This reverts commit 4cea702828a38e20797845dc3945dc6dc2f643b3.

--- CotEditor/Sources/CETextViewCore.m
+++ CotEditor/Sources/CETextViewCore.m
@@ -646,7 +646,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 // ------------------------------------------------------
 {
     NSDictionary *attrs = @{NSParagraphStyleAttributeName: [self paragraphStyle],
-                            NSFontAttributeName: [self font]};
+                            NSFontAttributeName: [self font],
+                            NSForegroundColorAttributeName: [self textColor]};
     
     [self setTypingAttributes:attrs];
 }
