34e714c98f5d0bcdda7ae021c9c25de755c639df
1024jp <1024jp@wolfrosch.com>
2014-04-23 21:42:12 +0200

    Prevent font panel setting text color to color panel (ref #109, #112)

--- CotEditor/Sources/CETextViewCore.m
+++ CotEditor/Sources/CETextViewCore.m
@@ -620,6 +620,18 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 }
 
 
+// ------------------------------------------------------
+/// フォントパネルを更新
+- (void)updateFontPanel
+// ------------------------------------------------------
+{
+    // フォントのみをフォントパネルに渡す
+    // -> super にやらせると、テキストカラーもフォントパネルに送り、フォントパネルがさらにカラーパネル（= カラーコードパネル）にそのテキストカラーを渡すので、
+    // それを断つために自分で渡す
+    [[NSFontPanel sharedFontPanel] setPanelFont:[self font] isMultiple:NO];
+}
+
+
 
 #pragma mark Public Methods
 
