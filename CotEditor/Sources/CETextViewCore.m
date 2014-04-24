d60d0e591518de026b3bb6d8acebd205c633c57c
1024jp <1024jp@wolfrosch.com>
2014-04-24 18:51:55 +0200

    Remove a duplicated line

--- CotEditor/Sources/CETextViewCore.m
+++ CotEditor/Sources/CETextViewCore.m
@@ -39,7 +39,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #import "CEEditorView.h"
 #import "CESyntaxManager.h"
 #import "CEColorCodePanelController.h"
-#import "CEColorCodePanelController.h"
 #import "CEKeyBindingManager.h"
 #import "constants.h"
 
