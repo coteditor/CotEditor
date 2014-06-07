775d9fafa311dcb8e24d57e993d725c4187cd8aa
1024jp <1024jp@wolfrosch.com>
2014-06-07 05:37:34 +0200

    Refactor around coloring indicator sheet
    カラーリングインジケータシートの管理を CEIndicatorSheetController に分離した。

--- CotEditor/Sources/CESyntax.h
+++ CotEditor/Sources/CESyntax.h
@@ -59,7 +59,4 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 - (void)colorVisibleRange:(NSRange)range withWholeString:(NSString *)wholeString;
 - (NSArray *)outlineMenuArrayWithWholeString:(NSString *)wholeString;
 
-// Action Message
-- (IBAction)cancelColoring:(id)sender;
-
 @end
