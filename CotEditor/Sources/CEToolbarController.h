60ac4d5f27072823984d059e83644f14614d2f58
1024jp <1024jp@wolfrosch.com>
2014-07-02 00:59:24 +0200

    Setup toolbar items in Interface Builder

--- CotEditor/Sources/CEToolbarController.h
+++ CotEditor/Sources/CEToolbarController.h
@@ -34,7 +34,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #import <Cocoa/Cocoa.h>
 
 
-@interface CEToolbarController : NSObject <NSToolbarDelegate>
+@interface CEToolbarController : NSObject
 
 // Public method
 - (void)updateToggleItem:(NSString *)identifer setOn:(BOOL)setOn;
--- CotEditor/Sources/CEToolbarController.m
+++ CotEditor/Sources/CEToolbarController.m
@@ -32,15 +32,14 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 #import "CEToolbarController.h"
-#import "CEDocumentController.h"
+#import "CEDocument.h"
 #import "constants.h"
 
 
 @interface CEToolbarController ()
 
-@property (nonatomic) IBOutlet NSToolbar *toolbar;
-
-@property (nonatomic, unsafe_unretained) IBOutlet NSWindow *mainWindow;  // NSWindow は 10.7 では weak で持てないため
+@property (nonatomic, weak) IBOutlet NSToolbar *toolbar;
+@property (nonatomic, unsafe_unretained) IBOutlet NSWindow *window;  // NSWindow は 10.7 では weak で持てないため
 @property (nonatomic, weak) IBOutlet NSPopUpButton *lineEndingPopupButton;
 @property (nonatomic, weak) IBOutlet NSPopUpButton *encodingPopupButton;
 @property (nonatomic, weak) IBOutlet NSPopUpButton *syntaxPopupButton;
@@ -176,6 +175,49 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 - (void)awakeFromNib
 // ------------------------------------------------------
 {
+    for (NSToolbarItem *item in [[self toolbar] items]) {
+        NSString *identifier = [item itemIdentifier];
+        NSString *toggleKey = nil;
+        
+        if ([identifier isEqualToString:k_showInvisibleCharsItemID]) {
+            // Show Invisible Characters
+            BOOL canActivate = [[[[self window] windowController] document] canActivateShowInvisibleCharsItem];
+            
+            // ツールバーアイテムを有効化できなければツールチップを変更
+            if (canActivate) {
+                [item setToolTip:NSLocalizedString(@"Show or hide invisible characters in text",@"")];
+                [self doUpdateToggleItem:item setOn:YES];
+                [item setAction:@selector(toggleShowInvisibleChars:)];
+            } else {
+                [item setToolTip:NSLocalizedString(@"To display invisible characters, set in Preferences and re-open the document.",@"")];
+                [self doUpdateToggleItem:item setOn:NO];
+                [item setAction:nil];
+            }
+            
+        } else if ([identifier isEqualToString:k_autoTabExpandItemID]) {
+            toggleKey = k_key_autoExpandTab;
+            
+        } else if ([identifier isEqualToString:k_showNavigationBarItemID]) {
+            toggleKey = k_key_showNavigationBar;
+            
+        } else if ([identifier isEqualToString:k_showLineNumItemID]) {
+            toggleKey = k_key_showLineNumbers;
+            
+        } else if ([identifier isEqualToString:k_showStatusBarItemID]) {
+            toggleKey = k_key_showStatusBar;
+            
+        } else if ([identifier isEqualToString:k_showPageGuideItemID]) {
+            toggleKey = k_key_showPageGuide;
+            
+        } else if ([identifier isEqualToString:k_wrapLinesItemID]) {
+            toggleKey = k_key_wrapLines;
+        }
+        
+        if (toggleKey) {
+            [self doUpdateToggleItem:item setOn:[[NSUserDefaults standardUserDefaults] boolForKey:toggleKey]];
+        }
+    }
+    
     [self buildEncodingPopupButton];
     [self buildSyntaxPopupButton];
     
@@ -188,236 +230,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
                                              selector:@selector(buildEncodingPopupButton)
                                                  name:CEEncodingListDidUpdateNotification
                                                object:nil];
-}
-
-
-
-#pragma mark Delegate and Notification
-
-//=======================================================
-// Delegate method (NSToolbar)
-//  <== toolbar
-//=======================================================
-
-// ------------------------------------------------------
-/// ツールバーアイテムを返す
-- (NSToolbarItem *)toolbar:(NSToolbar *)toolbar itemForItemIdentifier:(NSString *)itemIdentifier willBeInsertedIntoToolbar:(BOOL)flag
-// ------------------------------------------------------
-{
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    NSToolbarItem *toolbarItem = [[NSToolbarItem alloc] initWithItemIdentifier:itemIdentifier];
-
-    // Get Info (target = FirstResponder)
-    if ([itemIdentifier isEqualToString:k_getInfoItemID]) {
-        [toolbarItem setLabel:NSLocalizedString(@"Get Info",@"")];
-        [toolbarItem setPaletteLabel:NSLocalizedString(@"Get Info",@"")];
-        [toolbarItem setToolTip:NSLocalizedString(@"Show document information",@"")];
-        [toolbarItem setImage:[NSImage imageNamed:@"getInfo"]];
-        [toolbarItem setAction:@selector(getInfo:)];
-
-    // Show Incompatible Char (target = FirstResponder)
-    } else if ([itemIdentifier isEqualToString:k_showIncompatibleCharItemID]) {
-        [toolbarItem setLabel:NSLocalizedString(@"Incompatible Chars",@"")];
-        [toolbarItem setPaletteLabel:NSLocalizedString(@"Show Incompatible Chars",@"")];
-        [toolbarItem setToolTip:NSLocalizedString(@"Show incompatible chars for selected encoding",@"")];
-        [toolbarItem setImage:[NSImage imageNamed:@"IncompatibleChar"]];
-        [toolbarItem setAction:@selector(toggleIncompatibleCharList:)];
-
-    // Bigger Font
-    } else if ([itemIdentifier isEqualToString:k_biggerFontItemID]) {
-        [toolbarItem setLabel:NSLocalizedString(@"Bigger",@"")];
-        [toolbarItem setPaletteLabel:NSLocalizedString(@"Bigger Font",@"")];
-        [toolbarItem setToolTip:NSLocalizedString(@"Increases font size",@"")];
-        [toolbarItem setImage:[NSImage imageNamed:@"biggerFont"]];
-        [toolbarItem setTarget:[NSFontManager sharedFontManager]];
-        [toolbarItem setAction:@selector(modifyFont:)];
-        [toolbarItem setTag:NSSizeUpFontAction];
-
-    // Smaller Font
-    } else if ([itemIdentifier isEqualToString:k_smallerFontItemID]) {
-        [toolbarItem setLabel:NSLocalizedString(@"Smaller",@"")];
-        [toolbarItem setPaletteLabel:NSLocalizedString(@"Smaller Font",@"")];
-        [toolbarItem setToolTip:NSLocalizedString(@"Decreases font size",@"")];
-        [toolbarItem setImage:[NSImage imageNamed:@"smallerFont"]];
-        [toolbarItem setTarget:[NSFontManager sharedFontManager]];
-        [toolbarItem setAction:@selector(modifyFont:)];
-        [toolbarItem setTag:NSSizeDownFontAction];
-
-    // Shift Left (target = FirstResponder)
-    } else if ([itemIdentifier isEqualToString:k_shiftLeftItemID]) {
-        [toolbarItem setLabel:NSLocalizedString(@"Shift Left",@"")];
-        [toolbarItem setPaletteLabel:NSLocalizedString(@"Shift Left",@"")];
-        [toolbarItem setToolTip:NSLocalizedString(@"Shift lines to left",@"")];
-        [toolbarItem setImage:[NSImage imageNamed:@"Shift_Left"]];
-        [toolbarItem setAction:@selector(shiftLeft:)];
-
-    // Shift Right (target = FirstResponder)
-    } else if ([itemIdentifier isEqualToString:k_shiftRightItemID]) {
-        [toolbarItem setLabel:NSLocalizedString(@"Shift Right",@"")];
-        [toolbarItem setPaletteLabel:NSLocalizedString(@"Shift Right",@"")];
-        [toolbarItem setToolTip:NSLocalizedString(@"Shift lines to right",@"")];
-        [toolbarItem setImage:[NSImage imageNamed:@"Shift_Right"]];
-        [toolbarItem setAction:@selector(shiftRight:)];
-        
-        // Auto Tab Expand (target = FirstResponder)
-    } else if ([itemIdentifier isEqualToString:k_autoTabExpandItemID]) {
-        [toolbarItem setLabel:NSLocalizedString(@"Expand Tabs",@"")];
-        [toolbarItem setPaletteLabel:NSLocalizedString(@"Toggle Auto Tab Expand",@"")];
-        [toolbarItem setToolTip:NSLocalizedString(@"Toggle auto tab expand",@"")];
-        [self doUpdateToggleItem:toolbarItem setOn:[defaults boolForKey:k_key_autoExpandTab]];
-        [toolbarItem setAction:@selector(toggleAutoTabExpand:)];
-
-    // Show Navigation Bar (target = FirstResponder)
-    } else if ([itemIdentifier isEqualToString:k_showNavigationBarItemID]) {
-        [toolbarItem setLabel:NSLocalizedString(@"Navigation Bar",@"")];
-        [toolbarItem setPaletteLabel:NSLocalizedString(@"Show / Hide Navigation Bar",@"")];
-        [toolbarItem setToolTip:NSLocalizedString(@"Show or hide navigation bar of window",@"")];
-        [self doUpdateToggleItem:toolbarItem setOn:[defaults boolForKey:k_key_showNavigationBar]];
-        [toolbarItem setAction:@selector(toggleShowNavigationBar:)];
-
-    // Show Line Num (target = FirstResponder)
-    } else if ([itemIdentifier isEqualToString:k_showLineNumItemID]) {
-        [toolbarItem setLabel:NSLocalizedString(@"LineNum",@"")];
-        [toolbarItem setPaletteLabel:NSLocalizedString(@"Show / Hide Line Number",@"")];
-        [toolbarItem setToolTip:NSLocalizedString(@"Show or hide line number of text",@"")];
-        [self doUpdateToggleItem:toolbarItem setOn:[defaults boolForKey:k_key_showLineNumbers]];
-        [toolbarItem setAction:@selector(toggleShowLineNum:)];
-
-    // Show Status Bar (target = FirstResponder)
-    } else if ([itemIdentifier isEqualToString:k_showStatusBarItemID]) {
-        [toolbarItem setLabel:NSLocalizedString(@"Status Bar",@"")];
-        [toolbarItem setPaletteLabel:NSLocalizedString(@"Show / Hide Status Bar",@"")];
-        [toolbarItem setToolTip:NSLocalizedString(@"Show or hide status bar of window",@"")];
-        [self doUpdateToggleItem:toolbarItem setOn:[defaults boolForKey:k_key_showStatusBar]];
-        [toolbarItem setAction:@selector(toggleShowStatusBar:)];
-
-    // Show Invisible Characters (target = FirstResponder)
-    } else if ([itemIdentifier isEqualToString:k_showInvisibleCharsItemID]) {
-        BOOL canActivate = [[[[self mainWindow] windowController] document] canActivateShowInvisibleCharsItem];
-
-        [toolbarItem setLabel:NSLocalizedString(@"Invisible Chars",@"")];
-        [toolbarItem setPaletteLabel:NSLocalizedString(@"Show / Hide Invisible Chars",@"")];
-        // ツールバーアイテムを有効化できなければツールチップを変更
-        if (canActivate) {
-            [toolbarItem setToolTip:NSLocalizedString(@"Show or hide invisible characters in text",@"")];
-            [self doUpdateToggleItem:toolbarItem setOn:YES];
-            [toolbarItem setAction:@selector(toggleShowInvisibleChars:)];
-        } else {
-            [toolbarItem setToolTip:NSLocalizedString(@"To display invisible characters, set in Preferences and re-open the document.",@"")];
-            [self doUpdateToggleItem:toolbarItem setOn:NO];
-            [toolbarItem setAction:nil];
-        }
-
-    // Show Page Guide (target = FirstResponder)
-    } else if ([itemIdentifier isEqualToString:k_showPageGuideItemID]) {
-        [toolbarItem setLabel:NSLocalizedString(@"Page Guide",@"")];
-        [toolbarItem setPaletteLabel:NSLocalizedString(@"Show / Hide Page Guide",@"")];
-        [toolbarItem setToolTip:NSLocalizedString(@"Show or hide page guide line",@"")];
-        [self doUpdateToggleItem:toolbarItem setOn:[defaults boolForKey:k_key_showPageGuide]];
-        [toolbarItem setAction:@selector(toggleShowPageGuide:)];
-
-    // Wrap lines (target = FirstResponder)
-    } else if ([itemIdentifier isEqualToString:k_wrapLinesItemID]) {
-        [toolbarItem setLabel:NSLocalizedString(@"Wrap lines",@"")];
-        [toolbarItem setPaletteLabel:NSLocalizedString(@"Toggle Wrap Lines",@"")];
-        [toolbarItem setToolTip:NSLocalizedString(@"Toggle wrap lines",@"")];
-        [self doUpdateToggleItem:toolbarItem setOn:[defaults boolForKey:k_key_wrapLines]];
-        [toolbarItem setAction:@selector(toggleWrapLines:)];
-
-    // Line Endings (target = FirstResponder)
-    } else if ([itemIdentifier isEqualToString:k_lineEndingsItemID]) {
-        [toolbarItem setLabel:NSLocalizedString(@"Line Endings",@"")];
-        [toolbarItem setPaletteLabel:NSLocalizedString(@"Line Endings",@"")];
-        [toolbarItem setToolTip:NSLocalizedString(@"Line Endings",@"")];
-        [toolbarItem setView:[self lineEndingPopupButton]];
-        [toolbarItem setMinSize:[[self lineEndingPopupButton] bounds].size];
-        [toolbarItem setMaxSize:[[self lineEndingPopupButton] bounds].size];
-
-    // File Encoding (target = FirstResponder)
-    } else if ([itemIdentifier isEqualToString:k_fileEncodingsItemID]) {
-
-        [toolbarItem setLabel:NSLocalizedString(@"File Encoding",@"")];
-        [toolbarItem setPaletteLabel:NSLocalizedString(@"File Encoding",@"")];
-        [toolbarItem setToolTip:NSLocalizedString(@"File Encoding",@"")];
-        [toolbarItem setView:[self encodingPopupButton]];
-        [toolbarItem setMinSize:[[self encodingPopupButton] bounds].size];
-        [toolbarItem setMaxSize:[[self encodingPopupButton] bounds].size];
-
-    // Syntax Style (target = FirstResponder)
-    } else if ([itemIdentifier isEqualToString:k_syntaxItemID]) {
-
-        [toolbarItem setLabel:NSLocalizedString(@"Syntax Style",@"")];
-        [toolbarItem setPaletteLabel:NSLocalizedString(@"Syntax Style",@"")];
-        [toolbarItem setToolTip:NSLocalizedString(@"Syntax Style",@"")];
-        [toolbarItem setView:[self syntaxPopupButton]];
-        [toolbarItem setMinSize:[[self syntaxPopupButton] bounds].size];
-        [toolbarItem setMaxSize:[[self syntaxPopupButton] bounds].size];
-
-    // Re-color All (target = FirstResponder)
-    } else if ([itemIdentifier isEqualToString:k_syntaxReColorAllItemID]) {
-        [toolbarItem setLabel:NSLocalizedString(@"Re-Color",@"")];
-        [toolbarItem setPaletteLabel:NSLocalizedString(@"Re-Color All",@"")];
-        [toolbarItem setToolTip:NSLocalizedString(@"Do re-color whole document",@"")];
-        [toolbarItem setImage:[NSImage imageNamed:@"RecolorAll"]];
-        [toolbarItem setAction:@selector(recoloringAllStringOfDocument:)];
-        
-        // Edit Color Code (target = FirstResponder)
-    } else if ([itemIdentifier isEqualToString:k_editColorCodeItemID]) {
-        [toolbarItem setLabel:NSLocalizedString(@"Color Code",@"")];
-        [toolbarItem setPaletteLabel:NSLocalizedString(@"Edit Color Code",@"")];
-        [toolbarItem setToolTip:NSLocalizedString(@"Open Color Code Editor and set selection as color code",@"")];
-        [toolbarItem setImage:[NSImage imageNamed:@"EditColorCode"]];
-        [toolbarItem setAction:@selector(editColorCode:)];
-
-    } else {
-        toolbarItem = nil;
-    }
-    return toolbarItem;
-}
-
-
-// ------------------------------------------------------
-/// 設定画面でのツールバーアイテム配列を返す
-- (NSArray *)toolbarAllowedItemIdentifiers:(NSToolbar *)toolbar
-// ------------------------------------------------------
-{
-    return @[k_getInfoItemID, 
-             k_showIncompatibleCharItemID,
-             NSToolbarPrintItemIdentifier,  
-             NSToolbarShowFontsItemIdentifier, 
-             k_biggerFontItemID, 
-             k_smallerFontItemID, 
-             k_shiftLeftItemID, 
-             k_shiftRightItemID,
-             k_autoTabExpandItemID,
-             k_showNavigationBarItemID, 
-             k_showLineNumItemID, 
-             k_showStatusBarItemID, 
-             k_showInvisibleCharsItemID, 
-             k_showPageGuideItemID, 
-             k_wrapLinesItemID,
-             k_lineEndingsItemID, 
-             k_fileEncodingsItemID, 
-             k_syntaxItemID, 
-             k_syntaxReColorAllItemID,
-             k_editColorCodeItemID,
-             NSToolbarSeparatorItemIdentifier, 
-             NSToolbarFlexibleSpaceItemIdentifier, 
-             NSToolbarSpaceItemIdentifier, 
-             NSToolbarCustomizeToolbarItemIdentifier];
-}
-
-
-// ------------------------------------------------------
-/// ツールバーアイテムデフォルト配列を返す
-- (NSArray *)toolbarDefaultItemIdentifiers:(NSToolbar *)toolbar
-// ------------------------------------------------------
-{
-    return @[k_lineEndingsItemID, 
-             k_fileEncodingsItemID, 
-             k_syntaxItemID, 
-             NSToolbarFlexibleSpaceItemIdentifier, 
-             k_getInfoItemID];
 }
 
 
