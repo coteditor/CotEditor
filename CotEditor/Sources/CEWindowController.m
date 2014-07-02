121b6cf6416eef6be43ff0f6247bb636caeff89f
1024jp <1024jp@wolfrosch.com>
2014-07-02 02:16:56 +0200

    Remove unused class and reference

--- CotEditor/Sources/CEWindowController.m
+++ CotEditor/Sources/CEWindowController.m
@@ -60,7 +60,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 // readonly
 @property (nonatomic, weak, readwrite) IBOutlet CEEditorView *editorView;
-@property (nonatomic, weak, readwrite) IBOutlet CEToolbarController *toolbarController;
 
 @end
 
