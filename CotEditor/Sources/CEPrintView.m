510c7baf8dd0c7e9714906e47b457d487575ec56
1024jp <1024jp@wolfrosch.com>
2014-07-06 04:21:45 +0200

    Fix printView

--- CotEditor/Sources/CEPrintView.m
+++ CotEditor/Sources/CEPrintView.m
@@ -55,6 +55,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 @property (nonatomic) BOOL readyToDrawPageNum;
 @property (nonatomic) CGFloat xOffset;
 @property (nonatomic, copy) NSDictionary *headerFooterAttrs;
+@property (nonatomic) CESyntax *syntax;
 
 @end
 
