203962a8125a36c2fb3d02898bfbf4b095b6c572
1024jp <1024jp@wolfrosch.com>
2014-07-20 20:58:15 +0200

    Skip additional highlight rect calculation if line to draw is not last line (close #179)

--- CotEditor/Sources/CESubSplitView.m
+++ CotEditor/Sources/CESubSplitView.m
@@ -821,48 +821,61 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 // ------------------------------------------------------
 {
     if (![self highlightCurrentLine]) { return; }
-
-// IMでの仮名漢字変換中に背景色がカレント行ハイライト色でなくテキスト背景色になることがあるため、行の文字数の変化や追加描画矩形の
-// 比較では、描画をパスできない。
-// hasMarkedText を使っても、変換確定の直前直後の区別がつかないことがあるので、愚直に全工程を実行している。 2008.05.31.
+    
+    // 最初に（表示前に） TextView にテキストをセットした際にカーソルが一度文書末に行き、
+    // boundingRectForGlyphRange:inTextContainer: がムダに実行されるのを避ける (2014-07 by 1024jp)
+    if (![[self window] isVisible]) { return; }
+    
+    // IMでの仮名漢字変換中に背景色がカレント行ハイライト色でなくテキスト背景色になることがあるため、行の文字数の変化や追加描画矩形の
+    // 比較では、描画をパスできない。
+    // hasMarkedText を使っても、変換確定の直前直後の区別がつかないことがあるので、愚直に全工程を実行している。 2008.05.31.
 
     CELayoutManager *layoutManager = (CELayoutManager *)[[self textView] layoutManager];
+    NSRange lineRange = [[self string] lineRangeForRange:[[self textView] selectedRange]];
+    NSUInteger length = [[self string] length];
 
-// グリフがないときはそのまま戻ると、全くハイライトされないことと、アンドゥで描画が乱れるため、実行する。2008.06.21.
-//    NSUInteger numOfGlyphs = [layoutManager numberOfGlyphs];
-//    if (numOfGlyphs == 0) { return; }
-    NSRange selectedRange = [[self textView] selectedRange];
-    NSRange lineRange = [[self string] lineRangeForRange:selectedRange];
-
-    // 文字背景色でハイライトされる矩形を取得
-    NSRange glyphRange = [layoutManager glyphRangeForCharacterRange:lineRange actualCharacterRange:NULL];
-    NSRect attrsRect = [layoutManager boundingRectForGlyphRange:glyphRange
-                                                inTextContainer:[[self textView] textContainer]];
-    // 10.5.5でdeleteキーで最終行の文字をすべて削除すると、ハイライトが文頭に移動してしまう。これはboundingRectForGlyphRange:inTextContainer:でZeroRectが返ってきてしまうために起こる。extraLineFragmentRectも試したが、やはりZeroRectしか返ってこなかった。この不具合を迂回するための条件式を設定したが、最後に削除した文字列の背景部分のみハイライトしないままになる問題が残ってる。2008.11.15
-    if (([[self string] length] == 0) ||
-        ((!NSEqualRects(attrsRect, NSZeroRect)) && ([[self string] length] > 0))) {
-        // 文字背景色を塗っても右側に生じる「空白」の矩形を得る
-        CGFloat additionalWidth = [[[self textView] textContainer] containerSize].width
-                                  - attrsRect.size.width - attrsRect.origin.x
-                                  - [[self textView] textContainerInset].width
-                                  - [[[self textView] textContainer] lineFragmentPadding];
-        NSRect additionalRect = NSMakeRect(NSMaxX(attrsRect),
-                                           NSMinY(attrsRect) + [[self textView] textContainerOrigin].y, additionalWidth,
-                                           [layoutManager lineHeight]);
-    //NSLog(NSStringFromRange(glyphRange));
-    //NSLog(NSStringFromRect(attrsRect));
-    //NSLog(NSStringFromRange(lineRange));
-
-        // 追加描画矩形を描画する
-        if (!NSEqualRects([[self textView] highlightLineAdditionalRect], additionalRect)) {
-            [[self textView] setNeedsDisplayInRect:[[self textView] highlightLineAdditionalRect]];
-            [[self textView] setHighlightLineAdditionalRect:additionalRect];
-            [[self textView] setNeedsDisplayInRect:additionalRect];
+    // 最終行の場合は attributes だけでベタ塗りできないので追加矩形を描画
+    // (boundingRectForGlyphRange:inTextContainer: がとにかく重いので極力必要なときだけ実行する)
+    if (NSMaxRange(lineRange) == length) {
+        NSRect additionalRect;
+        CETextView *textView = [self textView];
+        
+        if (NSMaxRange([textView selectedRange]) == length && [layoutManager extraLineFragmentTextContainer]) {  // 最終行が空行の時
+            additionalRect = [layoutManager extraLineFragmentRect];
+            additionalRect.origin.x = [[textView textContainer] lineFragmentPadding];
+            additionalRect.origin.y += [textView textContainerOrigin].y;
+            additionalRect.size.width -= 2 * [[textView textContainer] lineFragmentPadding];
+            
+        } else {
+            // 文字背景色でハイライトされる矩形を取得
+            NSRange glyphRange = [layoutManager glyphRangeForCharacterRange:lineRange actualCharacterRange:NULL];
+            NSRect attrsRect = [layoutManager boundingRectForGlyphRange:glyphRange
+                                                        inTextContainer:[textView textContainer]];
+            // 文字背景色を塗っても右側に生じる「空白」の矩形を得る
+            CGFloat additionalWidth = [[textView textContainer] containerSize].width
+                                    - attrsRect.size.width - attrsRect.origin.x
+                                    - [textView textContainerInset].width
+                                    - [[textView textContainer] lineFragmentPadding];
+            additionalRect = NSMakeRect(NSMaxX(attrsRect),
+                                        NSMinY(attrsRect) + [textView textContainerOrigin].y,
+                                        additionalWidth,
+                                        [layoutManager lineHeight]);
         }
+        
+        // 追加描画矩形を描画する
+        if (!NSEqualRects([textView highlightLineAdditionalRect], additionalRect)) {
+            [textView setNeedsDisplayInRect:[textView highlightLineAdditionalRect]];
+            [textView setHighlightLineAdditionalRect:additionalRect];
+            [textView setNeedsDisplayInRect:additionalRect];
+        }
+        
+    } else {
+        // 最終行でないときは過去の追加矩形の削除のみしてあとは attributes にまかせる
+        [[self textView] setNeedsDisplayInRect:[[self textView] highlightLineAdditionalRect]];
+        [[self textView] setHighlightLineAdditionalRect:NSZeroRect];
     }
 
     // 古い範囲の文字背景色を削除し、新しい範囲にセット
-    NSDictionary *dict = @{NSBackgroundColorAttributeName: [[self textView] highlightLineColor]};
     // （文字列が削除されたときも実行されるので、範囲を検証しておかないと例外が発生する）
     NSRange removeAttrsRange = NSMakeRange(0, [[self textStorage] length]);
     
@@ -875,7 +888,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
     if (removeAttrsRange.length > 0) {
         [[self textStorage] removeAttribute:NSBackgroundColorAttributeName range:removeAttrsRange];
     }
-    [[self textStorage] addAttributes:dict range:lineRange];
+    [[self textStorage] addAttribute:NSBackgroundColorAttributeName value:[[self textView] highlightLineColor] range:lineRange];
     [[self textStorage] endEditing];
 }
 
