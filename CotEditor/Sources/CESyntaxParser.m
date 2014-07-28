77560b8190bf818c414944732279f06414469581
1024jp <1024jp@wolfrosch.com>
2014-07-28 06:05:30 +0200

    Remove unused DEBUG_macro.h

--- CotEditor/Sources/CESyntaxParser.m
+++ CotEditor/Sources/CESyntaxParser.m
@@ -36,7 +36,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #import "CESyntaxManager.h"
 #import "CEIndicatorSheetController.h"
 #import "RegexKitLite.h"
-#import "DEBUG_macro.h"
 #import "constants.h"
 
 
