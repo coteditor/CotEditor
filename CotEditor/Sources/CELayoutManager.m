ec7720c6d5e3a08955181c46b63bebf310ad3163
1024jp <1024jp@wolfrosch.com>
2014-08-03 02:14:14 +0200

    Beautify header comment

--- CotEditor/Sources/CELayoutManager.m
+++ CotEditor/Sources/CELayoutManager.m
@@ -1,41 +1,39 @@
 /*
-=================================================
-CELayoutManager
-(for CotEditor)
-
- Copyright (C) 2004-2007 nakamuxu.
- Copyright (C) 2014 CotEditor Project
- http://coteditor.github.io
-=================================================
-
-encoding="UTF-8"
-Created:2005.01.10
+ ==============================================================================
+ CELayoutManager
  
-------------
-This class is based on Smultron - SMLLayoutManager (written by Peter Borg – http://smultron.sourceforge.net)
-Smultron  Copyright (c) 2004 Peter Borg, All rights reserved.
-Smultron is released under GNU General Public License, http://www.gnu.org/copyleft/gpl.html
-arranged by nakamuxu, Jan 2005.
-arranged by 1024jp, Mar 2014.
--------------------------------------------------
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. 
-
-
-=================================================
-*/
+ CotEditor
+ http://coteditor.github.io
+ 
+ Created on 2005-01-10 by nakamuxu
+ encoding="UTF-8"
+ 
+ ------------
+ This class is based on Smultron - SMLLayoutManager (written by Peter Borg – http://smultron.sourceforge.net)
+ Smultron  Copyright (c) 2004 Peter Borg, All rights reserved.
+ Smultron is released under GNU General Public License, http://www.gnu.org/copyleft/gpl.html
+ arranged by nakamuxu, Jan 2005.
+ arranged by 1024jp, Mar 2014.
+ ------------------------------------------------------------------------------
+ 
+ © 2004-2007 nakamuxu
+ © 2014 CotEditor Project
+ 
+ This program is free software; you can redistribute it and/or modify it under
+ the terms of the GNU General Public License as published by the Free Software
+ Foundation; either version 2 of the License, or (at your option) any later
+ version.
+ 
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License along with
+ this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ Place - Suite 330, Boston, MA  02111-1307, USA.
+ 
+ ==============================================================================
+ */
 
 @import CoreText;
 #import "CELayoutManager.h"
--- CotEditor/Sources/CELineSpacingPanelController.h
+++ CotEditor/Sources/CELineSpacingPanelController.h
@@ -1,33 +1,30 @@
 /*
- =================================================
+ ==============================================================================
  CELineSpacingPanelController
- (for CotEditor)
  
- Copyright (C) 2014 CotEditor Project
+ CotEditor
  http://coteditor.github.io
- =================================================
  
+ Created by 2014-03-16 by 1024jp
  encoding="UTF-8"
- Created:2014-03-16 by 1024jp
+ ------------------------------------------------------------------------------
  
- -------------------------------------------------
+ © 2014 CotEditor Project
  
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License
- as published by the Free Software Foundation; either version 2
- of the License, or (at your option) any later version.
+ This program is free software; you can redistribute it and/or modify it under
+ the terms of the GNU General Public License as published by the Free Software
+ Foundation; either version 2 of the License, or (at your option) any later
+ version.
  
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ You should have received a copy of the GNU General Public License along with
+ this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ Place - Suite 330, Boston, MA  02111-1307, USA.
  
- 
- =================================================
+ ==============================================================================
  */
 
 #import "CEPanelController.h"
