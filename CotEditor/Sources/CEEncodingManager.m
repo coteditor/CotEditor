1bce1db5603b423564368676107e0a92927a4cd0
1024jp <1024jp@wolfrosch.com>
2015-01-20 22:59:40 +0100

    Use instancetype for singleton return

--- CotEditor/Sources/CEEncodingManager.m
+++ CotEditor/Sources/CEEncodingManager.m
@@ -10,7 +10,7 @@
  ------------------------------------------------------------------------------
  
  © 2004-2007 nakamuxu
- © 2014 CotEditor Project
+ © 2014-2015 1024jp
  
  This program is free software; you can redistribute it and/or modify it under
  the terms of the GNU General Public License as published by the Free Software
@@ -53,7 +53,7 @@ NSString *const CEEncodingListDidUpdateNotification = @"CESyntaxListDidUpdateNot
 
 // ------------------------------------------------------
 /// return singleton instance
-+ (CEEncodingManager *)sharedManager
++ (instancetype)sharedManager
 // ------------------------------------------------------
 {
     static dispatch_once_t onceToken;
