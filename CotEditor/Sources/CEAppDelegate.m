60dd3132b3bef1d4dcc9adef4c977d77492f54be
USAMI Kosuke <usami.kosuke@gmail.com>
2015-08-20 00:31:58 +0900

    Check beta updates (for beta release)

--- CotEditor/Sources/CEAppDelegate.m
+++ CotEditor/Sources/CEAppDelegate.m
@@ -97,7 +97,7 @@
     }
     
     NSDictionary *defaults = @{CEDefaultDocumentConflictOptionKey: @(CEDocumentConflictRevert),
-                               CEDefaultChecksUpdatesForBetaKey: @NO,
+                               CEDefaultChecksUpdatesForBetaKey: @YES,
                                CEDefaultLayoutTextVerticalKey: @NO,
                                CEDefaultSplitViewVerticalKey: @NO,
                                CEDefaultShowLineNumbersKey: @YES,
