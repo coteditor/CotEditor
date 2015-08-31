2a186fca39d818d67b02a863a3f9f12102f0ff08
USAMI Kosuke <usami.kosuke@gmail.com>
2015-08-31 21:13:28 +0900

    Merge release scripts

--- release/create_dmg_appcast.rb
+++ release/create_dmg_appcast.rb
@@ -10,15 +10,39 @@ unless Dir.exist?(app) then
 	exit
 end
 
+unless system "spctl -a -v #{app}" then
+	puts "Code sign error : #{app}"
+	exit
+end
+
 version = `/usr/libexec/PlistBuddy -c "Print :CFBundleVersion" #{app}/Contents/Info.plist`.chomp
 puts "Version #{version}"
 
 dmg = "./CotEditor_#{version}.dmg"
 
-unless File.exist?(dmg) then
-	puts "File not found : #{dmg}"
+if File.exist?(dmg) then
+	puts "Already exist : #{dmg}"
 	exit
 end
+
+# Create work directory
+dmgwork = "./CotEditor_#{version}"
+require 'fileutils'
+FileUtils.rm_rf(dmgwork)
+FileUtils.mkdir(dmgwork)
+FileUtils.mv(app, dmgwork)
+
+# Copy additional files
+files = "./files"
+if Dir.exist?(files) then
+	FileUtils.cp_r("#{files}/.", dmgwork)
+end
+
+# Create dmg
+system "hdiutil create -format UDBZ -srcfolder #{dmgwork} #{dmg}"
+FileUtils.rm_rf(dmgwork)
+
+puts "Created dmg for #{version}."
 
 
 if File.exist?(APPCAST_PATH) then
