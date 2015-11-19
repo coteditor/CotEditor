ebe43cb9b968460fabed7d53e428af445c7d9c2a
1024jp <1024jp@wolfrosch.com>
2015-11-19 19:54:38 +0900

    Add test for CECharacterInfo

--- Tests/CotEditorTests-Bridging-Header.h
+++ Tests/CotEditorTests-Bridging-Header.h
@@ -2,7 +2,11 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+// StringExtensionsTests
 #import "NSString+CENewLine.h"
 #import "NSString+ComposedCharacter.h"
 #import "NSString+JapaneseTransform.h"
 #import "NSString+Normalization.h"
+
+// CharacterInfoTests
+#import "CECharacterInfo.h"
