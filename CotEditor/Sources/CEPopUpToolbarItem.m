9ef80809b662cf89316e2e35565b0e86ee39d07b
1024jp <1024jp@wolfrosch.com>
2016-01-25 16:10:37 +0900

    Add an assertion to CEPopUpToolbarItem

--- CotEditor/Sources/CEPopUpToolbarItem.m
+++ CotEditor/Sources/CEPopUpToolbarItem.m
@@ -9,7 +9,7 @@
 
  ------------------------------------------------------------------------------
  
- © 2014 1024jp
+ © 2014-2016 1024jp
  
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
@@ -37,7 +37,8 @@
 - (void)awakeFromNib
 // ------------------------------------------------------
 {
-    if (![[self view] isKindOfClass:[NSPopUpButton class]]) { return; }
+    NSAssert([[self view] isKindOfClass:[NSPopUpButton class]],
+             @"%@'s view must be a kind of NSPopUpButton", [self className]);
     
     NSPopUpButton *popUpButton = (NSPopUpButton *)[self view];
     NSMenuItem *item = [[NSMenuItem alloc] init];
