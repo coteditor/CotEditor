55881427ceeb53b1c141ce566bf971947841e59e
1024jp <1024jp@wolfrosch.com>
2016-06-04 02:07:19 +0900

    Tweak weak text color

--- Tests/ThemeTests.swift
+++ Tests/ThemeTests.swift
@@ -66,7 +66,7 @@ class ThemeTests: XCTestCase {
         XCTAssertGreaterThan(theme.charactersColor.hueComponent, 0)
         XCTAssertGreaterThan(theme.commandsColor.hueComponent, 0)
         
-        XCTAssertEqualWithAccuracy(theme.weakTextColor.brightnessComponent, 0.3, accuracy: 0.01)
+        XCTAssertEqualWithAccuracy(theme.weakTextColor.brightnessComponent, 0.25, accuracy: 0.01)
         XCTAssertEqualWithAccuracy(theme.markupColor.brightnessComponent, 0.5, accuracy: 0.01)
         
         XCTAssertEqual(theme.syntaxColorForType(CEThemeKeywordsKey), theme.keywordsColor)
