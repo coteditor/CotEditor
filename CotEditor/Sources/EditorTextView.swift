c97860166cc4a4f80a932550c39f00f9ce932d26
1024jp <1024jp@wolfrosch.com>
2017-08-18 00:51:14 +0900

    Remove a workaround for fixed issue

--- CotEditor/Sources/EditorTextView.swift
+++ CotEditor/Sources/EditorTextView.swift
@@ -1118,8 +1118,7 @@ final class EditorTextView: NSTextView, Themable {
         //   -> The actual line height will be calculated in LayoutManager and ATSTypesetter based on this line height multiple.
         //      Because the default Cocoa Text System calculate line height differently
         //      if the first character of the document is drawn with another font (typically by a composite font).
-        //   -> Round line height for workaround to avoid expanding current line highlight when line height is 1.0. (2016-09 on macOS Sierra 10.12)
-        paragraphStyle.lineHeightMultiple = self.lineHeight.rounded(to: 5)
+        paragraphStyle.lineHeightMultiple = self.lineHeight
         
         // calculate tab interval
         if let font = self.font {
