4f9c6d22e178d7a3b3cb7f89d55a9e0649dad90f
1024jp <1024jp@wolfrosch.com>
2017-10-14 11:38:42 +0900

    Add a comment line in Document

--- CotEditor/Sources/Document.swift
+++ CotEditor/Sources/Document.swift
@@ -185,6 +185,7 @@ final class Document: NSDocument, AdditionalDocumentPreparing, EncodingHolder {
     /// enable asynchronous saving
     override func canAsynchronouslyWrite(to url: URL, ofType typeName: String, for saveOperation: NSSaveOperationType) -> Bool {
         
+        // -> Async-saving may cause an occasional crash. (2017-10)
         if NSAppKitVersion.current <= .macOS10_12 {
             return false
         }
