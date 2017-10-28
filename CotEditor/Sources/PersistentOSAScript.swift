d7950348b0956cdabd0f14d5437d7dfef353cbb3
1024jp <1024jp@wolfrosch.com>
2017-10-28 19:57:25 +0900

    Divide Sript.swift into 4 files

---
+++ CotEditor/Sources/PersistentOSAScript.swift
@@ -0,0 +1,100 @@
+/*
+ 
+ PersistentOSAScript.swift
+ 
+ CotEditor
+ https://coteditor.com
+ 
+ Created by 1024jp on 2017-10-28.
+ 
+ ------------------------------------------------------------------------------
+ 
+ © 2016-2017 1024jp
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ https://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ 
+ */
+
+import Foundation
+import OSAKit
+
+final class PersistentOSAScript: Script {
+    
+    // MARK: Script Properties
+    
+    let descriptor: ScriptDescriptor
+    let script: OSAScript
+    
+    
+    
+    // MARK: -
+    // MARK: Lifecycle
+    
+    init?(descriptor: ScriptDescriptor) {
+        
+        guard let script = OSAScript(contentsOf: descriptor.url, error: nil) else { return nil }
+        
+        self.descriptor = descriptor
+        self.script = script
+    }
+    
+    
+    
+    // MARK: Script Methods
+    
+    /// run script
+    /// - throws: Error by NSUserScriptTask
+    func run(completionHandler: (() -> Void)? = nil) throws {
+        
+        guard self.descriptor.url.isReachable else {
+            throw ScriptFileError(kind: .existance, url: self.descriptor.url)
+        }
+        
+        var errorInfo: NSDictionary? = NSDictionary()
+        if self.script.executeAndReturnError(&errorInfo) == nil {
+            let message = (errorInfo?[NSLocalizedDescriptionKey] as? String) ?? "Unknown error"
+            writeToConsole(message: message, scriptName: self.descriptor.name)
+        }
+        
+        completionHandler?()
+    }
+    
+    
+    /// Execute the AppleScript script by sending it the given Apple event.
+    ///
+    /// Any script errors will be written to the console panel.
+    ///
+    /// - parameter event: the apple event
+    ///
+    /// - throws: `ScriptFileError` and any errors by `NSUserScriptTask.init(url:)`
+    func run(withAppleEvent event: NSAppleEventDescriptor?, completionHandler: (() -> Void)? = nil) throws {
+        
+        guard let event = event else {
+            try self.run(completionHandler: completionHandler)
+            return
+        }
+        
+        guard self.descriptor.url.isReachable else {
+            throw ScriptFileError(kind: .existance, url: self.descriptor.url)
+        }
+        
+        var errorInfo: NSDictionary?
+        if self.script.executeAppleEvent(event, error: &errorInfo) == nil {
+            let message = (errorInfo?[NSLocalizedDescriptionKey] as? String) ?? "Unknown error"
+            writeToConsole(message: message, scriptName: self.descriptor.name)
+        }
+        
+        completionHandler?()
+    }
+    
+}
---
+++ CotEditor/Sources/ShellScript.swift
@@ -0,0 +1,311 @@
+/*
+ 
+ ShellScript.swift
+ 
+ CotEditor
+ https://coteditor.com
+ 
+ Created by 1024jp on 2017-10-28.
+ 
+ ------------------------------------------------------------------------------
+ 
+ © 2004-2007 nakamuxu
+ © 2014-2017 1024jp
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ https://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ 
+ */
+
+import Cocoa
+
+final class ShellScript: Script {
+    
+    // MARK: Script Properties
+    
+    let descriptor: ScriptDescriptor
+    
+    
+    
+    // MARK: -
+    // MARK: Lifecycle
+    
+    init(with descriptor: ScriptDescriptor) {
+        
+        self.descriptor = descriptor
+    }
+    
+    
+    
+    // MARK: Private Enum
+    
+    private enum OutputType: String, ScriptToken {
+        
+        case replaceSelection = "ReplaceSelection"
+        case replaceAllText = "ReplaceAllText"
+        case insertAfterSelection = "InsertAfterSelection"
+        case appendToAllText = "AppendToAllText"
+        case newDocument = "NewDocument"
+        case pasteBoard = "Pasteboard"
+        
+        static var token = "CotEditorXOutput"
+    }
+    
+    
+    private enum InputType: String, ScriptToken {
+        
+        case selection = "Selection"
+        case allText = "AllText"
+        
+        static var token = "CotEditorXInput"
+    }
+    
+    
+    
+    // MARK: Script Methods
+    
+    /// run script
+    /// - throws: ScriptFileError or Error by NSUserScriptTask
+    func run(completionHandler: (() -> Void)? = nil) throws {
+        
+        // check script file
+        guard self.descriptor.url.isReachable else {
+            throw ScriptFileError(kind: .existance, url: self.descriptor.url)
+        }
+        guard try self.descriptor.url.resourceValues(forKeys: [.isExecutableKey]).isExecutable ?? false else {
+            throw ScriptFileError(kind: .permission, url: self.descriptor.url)
+        }
+        guard let script = self.content, !script.isEmpty else {
+            throw ScriptFileError(kind: .read, url: self.descriptor.url)
+        }
+        
+        // fetch target document
+        weak var document = NSDocumentController.shared.currentDocument as? Document
+        
+        // read input
+        let input: String?
+        if let inputType = InputType(scanning: script) {
+            do {
+                input = try self.readInputString(type: inputType, editor: document)
+            } catch {
+                writeToConsole(message: error.localizedDescription, scriptName: self.descriptor.name)
+                return
+            }
+        } else {
+            input = nil
+        }
+        
+        // get output type
+        let outputType = OutputType(scanning: script)
+        
+        // prepare file path as argument if available
+        let arguments: [String] = [document?.fileURL?.path].flatMap { $0 }
+        
+        // create task
+        let task = try NSUserUnixTask(url: self.descriptor.url)
+        
+        // set pipes
+        let inPipe = Pipe()
+        let outPipe = Pipe()
+        let errPipe = Pipe()
+        task.standardInput = inPipe.fileHandleForReading
+        task.standardOutput = outPipe.fileHandleForWriting
+        task.standardError = errPipe.fileHandleForWriting
+        
+        // set input data if available
+        if let data = input?.data(using: .utf8) {
+            inPipe.fileHandleForWriting.write(data)
+            inPipe.fileHandleForWriting.closeFile()
+        }
+        
+        let scriptName = self.descriptor.name
+        var isCancelled = false  // user cancel state
+        
+        // read output asynchronously for safe with huge output
+        if let outputType = outputType {
+            outPipe.fileHandleForReading.readToEndOfFileInBackgroundAndNotify()
+            weak var observer: NSObjectProtocol?
+            observer = NotificationCenter.default.addObserver(forName: .NSFileHandleReadToEndOfFileCompletion, object: outPipe.fileHandleForReading, queue: nil) { (note: Notification) in
+                NotificationCenter.default.removeObserver(observer!)
+                
+                guard
+                    !isCancelled,
+                    let data = note.userInfo?[NSFileHandleNotificationDataItem] as? Data,
+                    let output = String(data: data, encoding: .utf8)
+                    else { return }
+                
+                do {
+                    try ShellScript.applyOutput(output, editor: document, type: outputType)
+                } catch {
+                    writeToConsole(message: error.localizedDescription, scriptName: scriptName)
+                }
+            }
+        }
+        
+        // execute
+        task.execute(withArguments: arguments) { error in
+            defer {
+                completionHandler?()
+            }
+            
+            // on user cancel
+            if let error = error as? POSIXError, error.code == .ENOTBLK {
+                isCancelled = true
+                return
+            }
+            
+            // put error message on the console
+            let errorData = errPipe.fileHandleForReading.readDataToEndOfFile()
+            if let message = String(data: errorData, encoding: .utf8), !message.isEmpty {
+                writeToConsole(message: message, scriptName: scriptName)
+            }
+        }
+    }
+    
+    
+    // MARK: Private Methods
+    
+    /// read content of script file
+    private lazy var content: String? = {
+        
+        guard let data = try? Data(contentsOf: self.descriptor.url) else { return nil }
+        
+        for encoding in EncodingManager.shared.defaultEncodings {
+            guard let encoding = encoding else { continue }
+            
+            if let contentString = String(data: data, encoding: encoding) {
+                return contentString
+            }
+        }
+        
+        return nil
+    }()
+    
+    
+    /// return document content conforming to the input type
+    /// - throws: ScriptError
+    private func readInputString(type: InputType, editor: Editable?) throws -> String {
+        
+        guard let editor = editor else {
+            throw ScriptError.noInputTarget
+        }
+        
+        switch type {
+        case .selection:
+            return editor.selectedString
+            
+        case .allText:
+            return editor.string
+        }
+    }
+    
+    
+    /// apply results conforming to the output type to the frontmost document
+    /// - throws: ScriptError
+    private static func applyOutput(_ output: String, editor: Editable?, type: OutputType) throws {
+        
+        if type == .pasteBoard {
+            let pasteboard = NSPasteboard.general
+            pasteboard.declareTypes([.string], owner: nil)
+            guard pasteboard.setString(output, forType: .string) else {
+                NSSound.beep()
+                return
+            }
+            return
+        }
+        
+        if type == .newDocument {
+            let document = try NSDocumentController.shared.openUntitledDocumentAndDisplay(true) as! Document
+            document.insert(string: output)
+            document.selectedRange = NSRange(location: 0, length: 0)
+            return
+        }
+        
+        guard let editor = editor else {
+            throw ScriptError.noOutputTarget
+        }
+        
+        DispatchQueue.main.async {
+            switch type {
+            case .replaceSelection:
+                editor.insert(string: output)
+                
+            case .replaceAllText:
+                editor.replaceAllString(with: output)
+                
+            case .insertAfterSelection:
+                editor.insertAfterSelection(string: output)
+                
+            case .appendToAllText:
+                editor.append(string: output)
+                
+            case .newDocument, .pasteBoard:
+                assertionFailure()
+            }
+        }
+    }
+    
+}
+
+
+
+
+// MARK: - Error
+
+private enum ScriptError: Error {
+    
+    case noInputTarget
+    case noOutputTarget
+    
+    
+    var localizedDescription: String {
+        
+        switch self {
+        case .noInputTarget:
+            return NSLocalizedString("No document to get input.", comment: "")
+        case .noOutputTarget:
+            return NSLocalizedString("No document to put output.", comment: "")
+        }
+    }
+    
+}
+
+
+
+
+// MARK: - ScriptToken
+
+private protocol ScriptToken {
+    
+    static var token: String { get }
+    
+    init?(rawValue: String)
+    
+}
+
+
+private extension ScriptToken {
+    
+    /// read type from script
+    init?(scanning script: String) {
+        
+        let pattern = "%%%\\{" + Self.token + "=" + "(.+)" + "\\}%%%"
+        let regex = try! NSRegularExpression(pattern: pattern)
+        
+        guard let result = regex.firstMatch(in: script, range: script.nsRange) else { return nil }
+        
+        let type = (script as NSString).substring(with: result.range(at: 1))
+        
+        self.init(rawValue: type)
+    }
+    
+}
