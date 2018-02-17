3d3f8b2013289cd67ce92a63849429df593bdc73
1024jp <1024jp@wolfrosch.com>
2018-02-18 00:50:09 +0900

    Update copyright year of a file  (#783)

--- CotEditor/Sources/WindowContentViewController.swift
+++ CotEditor/Sources/WindowContentViewController.swift
@@ -9,7 +9,7 @@
  
  ------------------------------------------------------------------------------
  
- © 2016-2017 1024jp
+ © 2016-2018 1024jp
  
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
