d46585a0712216d750b104ec38df4e490a942ac5
1024jp <1024jp@wolfrosch.com>
2018-02-23 10:14:58 +0900

    Install Differ framework

---
+++ Carthage/Build/Mac/Differ.framework/Versions/A/Headers/Differ.h
@@ -0,0 +1,7 @@
+@import Foundation;
+
+//! Project version number for Differ.
+FOUNDATION_EXPORT double DifferVersionNumber;
+
+//! Project version string for Differ.
+FOUNDATION_EXPORT const unsigned char DifferVersionString[];
