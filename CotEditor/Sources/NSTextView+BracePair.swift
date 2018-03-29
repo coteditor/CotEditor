d29b3d863ce682381bc2ced5c269194fe6f2dcf7
1024jp <1024jp@wolfrosch.com>
2018-03-29 12:44:39 +0900

    Extact NSTextView extension for brace pairs (#807)

---
+++ CotEditor/Sources/NSTextView+BracePair.swift
@@ -0,0 +1,60 @@
+//
+//  NSTextView+BracePair.swift
+//
+//  CotEditor
+//  https://coteditor.com
+//
+//  Created by 1024jp on 2018-03-29.
+//
+//  ---------------------------------------------------------------------------
+//
+//  © 2018 1024jp
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  https://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Cocoa
+
+extension NSTextView {
+    
+    /// find the matching brace for the character before the cursor and highlight it
+    func highligtMatchingBrace(candidates: [BracePair]) {
+        
+        let string = self.string
+        let selectedRange = self.selectedRange
+        
+        guard
+            !string.isEmpty,
+            selectedRange.length == 0,
+            selectedRange.location != NSNotFound,
+            selectedRange.location > 0,
+            let cursorIndex = Range(selectedRange, in: string)?.lowerBound
+            else { return }
+        
+        // check the character just before the cursor
+        let lastIndex = string.index(before: cursorIndex)
+        let lastCharacter = string[lastIndex]
+        
+        guard
+            let pair = candidates.first(where: { $0.begin == lastCharacter || $0.end == lastCharacter }),
+            let index = (pair.begin == lastCharacter)
+                ? string.indexOfEndBrace(for: pair, at: lastIndex)
+                : string.indexOfBeginBrace(for: pair, at: lastIndex)
+            else { return }
+        
+        let range = NSRange(index...index, in: string)
+        
+        self.showFindIndicator(for: range)
+    }
+    
+}
