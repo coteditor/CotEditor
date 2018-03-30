84a26351689145b18c17834efd41ee9ff71ce448
1024jp <1024jp@wolfrosch.com>
2018-03-30 15:29:09 +0900

    Change behvior of `⌘9` to select the last tab (#789)

--- CotEditor/Sources/AlphaWindow.swift
+++ CotEditor/Sources/AlphaWindow.swift
@@ -9,7 +9,7 @@
  
  ------------------------------------------------------------------------------
  
- © 2014-2017 1024jp
+ © 2014-2018 1024jp
  
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
@@ -158,9 +158,9 @@ extension AlphaWindow {
             #available(macOS 10.12, *),
             event.modifierFlags.intersection(.deviceIndependentFlagsMask).subtracting(.numericPad) == .command,
             let characters = event.charactersIgnoringModifiers,
-            let number = Int(characters),
+            let number = Int(characters), number > 0,
             let windows = self.tabbedWindows,
-            let window = (number == 0) ? windows.last : windows[safe: number - 1]  // 1-based to 0-based
+            let window = (number == 9) ? windows.last : windows[safe: number - 1]  // 1-based to 0-based
             else { return false }
         
         // prefer existing shortcut that user might define
