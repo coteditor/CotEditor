93cc16ad82f245816580691b76d6897d80d375f6
1024jp <1024jp@wolfrosch.com>
2018-04-01 15:35:31 +0900

    Update file header style

--- CotEditor/Sources/HUDController.swift
+++ CotEditor/Sources/HUDController.swift
@@ -1,29 +1,27 @@
-/*
- 
- HUDController.swift
- 
- CotEditor
- https://coteditor.com
- 
- Created by 1024jp on 2016-01-13.
- 
- ------------------------------------------------------------------------------
- 
- © 2016-2017 1024jp
- 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- 
- https://www.apache.org/licenses/LICENSE-2.0
- 
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- 
- */
+//
+//  HUDController.swift
+//
+//  CotEditor
+//  https://coteditor.com
+//
+//  Created by 1024jp on 2016-01-13.
+//
+//  ---------------------------------------------------------------------------
+//
+//  © 2016-2018 1024jp
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  https://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
 
 import Cocoa
 import QuartzCore
@@ -33,6 +31,7 @@ enum HUDSymbol {
     case wrap
     
     var image: NSImage {
+        
         switch self {
         case .wrap:
             return #imageLiteral(resourceName: "WrapTemplate")
--- Tests/StringIndentationTests.swift
+++ Tests/StringIndentationTests.swift
@@ -1,30 +1,28 @@
-/*
- 
- StringIndentationTests.swift
- Tests
- 
- CotEditor
- https://coteditor.com
- 
- Created by 1024jp on 2015-11-24.
- 
- ------------------------------------------------------------------------------
- 
- © 2015-2016 1024jp
- 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- 
- https://www.apache.org/licenses/LICENSE-2.0
- 
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- 
- */
+//
+//  StringIndentationTests.swift
+//  Tests
+//
+//  CotEditor
+//  https://coteditor.com
+//
+//  Created by 1024jp on 2015-11-24.
+//
+//  ---------------------------------------------------------------------------
+//
+//  © 2015-2018 1024jp
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  https://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
 
 import XCTest
 @testable import CotEditor
