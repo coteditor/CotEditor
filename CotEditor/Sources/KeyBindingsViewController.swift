0233d8e39960435a77ff4d334e7a848f20ce1207
1024jp <1024jp@wolfrosch.com>
2019-05-02 08:52:12 +0900

    Reload snippet setting every time when before showing editor (#951)

--- CotEditor/Sources/KeyBindingsViewController.swift
+++ CotEditor/Sources/KeyBindingsViewController.swift
@@ -8,7 +8,7 @@
 //
 //  ---------------------------------------------------------------------------
 //
-//  © 2014-2018 1024jp
+//  © 2014-2019 1024jp
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -65,9 +65,9 @@ class KeyBindingsViewController: NSViewController, NSOutlineViewDataSource, NSOu
     // MARK: -
     // MARK: View Controller Methods
     
-    override func viewDidLoad() {
+    override func viewWillAppear() {
         
-        super.viewDidLoad()
+        super.viewWillAppear()
         
         self.outlineTree = self.manager.outlineTree(defaults: false)
         self.isRestoreble = !self.manager.usesDefaultKeyBindings
