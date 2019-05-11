a14d231c275c28a93ba8d99710fd5167b4b071e8
1024jp <1024jp@wolfrosch.com>
2019-05-12 01:29:15 +0900

    Update copyright year of a source file

--- CotEditor/Sources/UnicodeScalar+ControlCharacter.swift
+++ CotEditor/Sources/UnicodeScalar+ControlCharacter.swift
@@ -8,7 +8,7 @@
 //
 //  ---------------------------------------------------------------------------
 //
-//  © 2015-2018 1024jp
+//  © 2015-2019 1024jp
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
