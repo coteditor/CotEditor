e6e80c1ea196b630d9682f24ab272c16d5668e1e
1024jp <1024jp@wolfrosch.com>
2020-01-18 17:35:37 +0900

    Make some IBOutlets weak

--- CotEditor/Sources/FindPanelContentViewController.swift
+++ CotEditor/Sources/FindPanelContentViewController.swift
@@ -8,7 +8,7 @@
 //
 //  ---------------------------------------------------------------------------
 //
-//  © 2014-2019 1024jp
+//  © 2014-2020 1024jp
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -33,8 +33,8 @@ final class FindPanelContentViewController: NSSplitViewController, TextFinderDel
     
     private var isUncollapsing = false
     
-    @IBOutlet private var fieldSplitViewItem: NSSplitViewItem?
-    @IBOutlet private var resultSplitViewItem: NSSplitViewItem?
+    @IBOutlet private weak var fieldSplitViewItem: NSSplitViewItem?
+    @IBOutlet private weak var resultSplitViewItem: NSSplitViewItem?
     
     
     
