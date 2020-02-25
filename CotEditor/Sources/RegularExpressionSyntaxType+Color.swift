fb41f0f49c49df92c54e2588c524753dba2d7a9d
1024jp <1024jp@wolfrosch.com>
2020-02-25 13:19:39 +0900

    Remove meanless import

--- CotEditor/Sources/RegularExpressionSyntaxType+Color.swift
+++ CotEditor/Sources/RegularExpressionSyntaxType+Color.swift
@@ -8,7 +8,7 @@
 //
 //  ---------------------------------------------------------------------------
 //
-//  © 2018-2019 1024jp
+//  © 2018-2020 1024jp
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -23,7 +23,6 @@
 //  limitations under the License.
 //
 
-import Foundation
 import AppKit.NSColor
 
 extension RegularExpressionSyntaxType {
