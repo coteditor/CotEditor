2bfcce4a86911c79e051b8e3b61dab0d5d9f20e4
1024jp <1024jp@wolfrosch.com>
2020-03-01 02:05:13 +0900

    Use colorSpaxce instead of colorSpaceName

--- CotEditor/Sources/Theme.swift
+++ CotEditor/Sources/Theme.swift
@@ -8,7 +8,7 @@
 //
 //  ---------------------------------------------------------------------------
 //
-//  © 2014-2019 1024jp
+//  © 2014-2020 1024jp
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -40,7 +40,7 @@ final class Theme: NSObject {
         
         @objc dynamic var color: NSColor
         
-        fileprivate static let invalidColor = NSColor.gray.usingColorSpaceName(.calibratedRGB)!
+        fileprivate static let invalidColor = NSColor.gray.usingColorSpace(.genericRGB)!
         
         
         init(color: NSColor) {
