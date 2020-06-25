67ff5b4c244be33044b8c0ac58dbdcc8a29ab99a
1024jp <1024jp@wolfrosch.com>
2020-06-25 14:33:41 +0900

    Revert "Simplify SelectionColorWell"
    This reverts commit 6fcac9a3d981e54aaa8980b8b40694ab8ee1ce41.

--- CotEditor/Sources/SelectionColorWell.swift
+++ CotEditor/Sources/SelectionColorWell.swift
@@ -8,7 +8,7 @@
 //
 //  ---------------------------------------------------------------------------
 //
-//  © 2018-2020 1024jp
+//  © 2018 1024jp
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -29,13 +29,23 @@ final class SelectionColorWell: NSColorWell {
     
     // MARK: Color Well Methods
     
-    override var isEnabled: Bool {
+    override func viewWillDraw() {
         
-        didSet {
-            if !isEnabled {
-                self.color = .selectedTextBackgroundColor
-            }
-        }
+        super.viewWillDraw()
+        
+        self.invalidateColor()
+    }
+    
+    
+    
+    // MARK: Private Methods
+    
+    /// apply system selection color if disabled
+    private func invalidateColor() {
+        
+        guard !self.isEnabled else { return }
+        
+        self.color = .selectedTextBackgroundColor
     }
     
 }
