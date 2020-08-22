afc8cd0852f7f69cb9e276a36505fa505fdae7cd
1024jp <1024jp@wolfrosch.com>
2020-08-22 15:57:21 +0900

    Update autolayout constrains in integration pane

--- CotEditor/Sources/IntegrationPaneController.swift
+++ CotEditor/Sources/IntegrationPaneController.swift
@@ -8,7 +8,7 @@
 //
 //  ---------------------------------------------------------------------------
 //
-//  © 2014-2019 1024jp
+//  © 2014-2020 1024jp
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -29,24 +29,8 @@ final class IntegrationPaneController: NSViewController {
     
     // MARK: Private Properties
     
-    private static let commandURL = Bundle.main.sharedSupportURL!.appendingPathComponent("bin/cot").standardizedFileURL
-    
-    private static let preferredLinkURL = URL(fileURLWithPath: "/usr/local/bin/cot")
-    
-    private static let preferredLinkTargetURL: URL = {
-        // cot in CotEditor.app in /Applications directory
-        let applicationDirURL = try! FileManager.default.url(for: .applicationDirectory,
-                                                             in: .localDomainMask,
-                                                             appropriateFor: nil,
-                                                             create: false)
-        let appName = Bundle.main.bundleName
-        
-        return applicationDirURL.appendingPathComponent(appName).appendingPathExtension("app")
-    }()
-    
-    @objc private dynamic var linkURL: URL!
-    @objc private dynamic var installed = false
-    @objc private dynamic var warning: String?
+    @IBOutlet private weak var cltStatusView: NSImageView?
+    @IBOutlet private weak var cltPathField: NSTextField?
     
     
     
@@ -58,46 +42,24 @@ final class IntegrationPaneController: NSViewController {
         
         super.viewWillAppear()
         
-        self.installed = self.validateSymLink()
+        self.validateCommandLineTool()
     }
     
     
     
     // MARK: Private Methods
     
-    /// check the destination of symlink and return whether 'cot' command is exists at '/usr/local/bin/'
-    private func validateSymLink() -> Bool {
+    private func validateCommandLineTool() {
         
-        // reset once
-        self.warning = nil
+        let status = CommandLineToolManager.shared.validateSymLink()
         
-        // check only preferred link location
-        self.linkURL = Self.preferredLinkURL
+        self.cltStatusView?.isHidden = !status.installed
+        self.cltStatusView?.image = status.badge.image
+        self.cltStatusView?.toolTip = status.message
         
-        // not installed yet (= can install)
-        if !self.linkURL.isReachable { return false }
-        
-        let linkDestinationURL = self.linkURL.resolvingSymlinksInPath()
-        
-        // treat symlink as "installed"
-        if linkDestinationURL == self.linkURL { return true }
-        
-        // link to bundled cot is, of course, valid
-        if linkDestinationURL == Self.commandURL { return true }
-        
-        // link to '/Applications/CotEditor.app' is always valid
-        if linkDestinationURL == Self.preferredLinkTargetURL { return true }
-        
-        // display warning for invalid link
-        if linkDestinationURL.isReachable {
-            // link destination is not running CotEditor
-            self.warning = "The current 'cot' symbolic link doesn’t target the running CotEditor.".localized
-        } else {
-            // link destination is unreachable
-            self.warning = "The current 'cot' symbolic link may target an invalid path.".localized
-        }
-        
-        return true
+        self.cltPathField?.isHidden = !status.installed
+        self.cltPathField?.stringValue = String(format: "installed at %@".localized,
+                                                CommandLineToolManager.shared.linkURL.path)
     }
     
 }
