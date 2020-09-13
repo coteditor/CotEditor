b9280a344c691836a7827a2cb5e4694fc0f07f14
1024jp <1024jp@wolfrosch.com>
2020-09-13 18:31:26 +0900

    Optimize drawCurrentLine(in:)

--- CotEditor/Sources/NSTextView+CurrentLineHighlighting.swift
+++ CotEditor/Sources/NSTextView+CurrentLineHighlighting.swift
@@ -46,17 +46,21 @@ extension CurrentLineHighlighting {
             self.needsUpdateLineHighlight = false
         }
         
-        guard let color = self.lineHighLightColor else { return }
-        
-        NSGraphicsContext.saveGraphicsState()
-        
-        color.setFill()
-        self.lineHighLightRects
+        let rects = self.lineHighLightRects
             .filter { $0.intersects(dirtyRect) }
             .map { self.centerScanRect($0) }
             .compactMap { $0.intersection(dirtyRect) }
-            .forEach { $0.fill() }
         
+        guard
+            !rects.isEmpty,
+            let color = self.lineHighLightColor
+            else { return }
+        
+        NSGraphicsContext.saveGraphicsState()
+        color.setFill()
+        for rect in rects {
+            rect.fill()
+        }
         NSGraphicsContext.restoreGraphicsState()
     }
     
