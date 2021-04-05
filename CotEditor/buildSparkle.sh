da11c8a414dc729ba3333dd9bd3d4d7fa33ac520
1024jp <1024jp@wolfrosch.com>
2021-04-05 13:17:18 +0900

    Omit optional Sparkle XPCs
    https://github.com/sparkle-project/Sparkle/issues/1814

--- CotEditor/buildSparkle.sh
+++ CotEditor/buildSparkle.sh
@@ -9,7 +9,7 @@
 #
 #  ------------------------------------------------------------------------------
 #
-#  © 2018-2020 1024jp
+#  © 2018-2021 1024jp
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -35,8 +35,6 @@ xcodebuild -scheme Distribution -configuration Release -derivedDataPath $derived
 mkdir -p ../Build
 items=(
     'org.sparkle-project.InstallerLauncher.xpc'
-    'org.sparkle-project.InstallerConnection.xpc'
-    'org.sparkle-project.InstallerStatus.xpc'
     'Sparkle.framework'
 )
 for item in ${items[@]}; do
