4d8cdfc6636aab41dd2047fab3b83980509e1c4c
1024jp <1024jp@wolfrosch.com>
2021-06-17 10:16:11 +0900

    Update assertion message

--- CotEditor/Sources/SyntaxParser.swift
+++ CotEditor/Sources/SyntaxParser.swift
@@ -9,7 +9,7 @@
 //  ---------------------------------------------------------------------------
 //
 //  © 2004-2007 nakamuxu
-//  © 2014-2020 1024jp
+//  © 2014-2021 1024jp
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -163,7 +163,7 @@ extension SyntaxParser {
         
         // in case that wholeRange length is changed from editedRange
         guard editedRange.flatMap({ $0.upperBound > self.textStorage.length }) != true else {
-            assertionFailure("Invalid range is passed in to \(#function)")
+            assertionFailure("Invalid range \(editedRange?.description ?? "nil") is passed in to \(#function)")
             return nil
         }
         
