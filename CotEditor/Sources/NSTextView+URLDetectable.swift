c5e9f6b0c8446aa3188371cf5f059619b4003f97
1024jp <1024jp@wolfrosch.com>
2022-01-24 13:50:27 +0900

    Use Task for async URL detection

--- CotEditor/Sources/NSTextView+URLDetectable.swift
+++ CotEditor/Sources/NSTextView+URLDetectable.swift
@@ -8,7 +8,7 @@
 //
 //  ---------------------------------------------------------------------------
 //
-//  © 2020 1024jp
+//  © 2020-2022 1024jp
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -27,94 +27,45 @@ import Cocoa
 
 protocol URLDetectable: NSTextView {
     
-    var urlDetectionQueue: OperationQueue { get }
+    var urlDetectionTask: Task<Void, Error>? { get set }
 }
 
 
 extension URLDetectable {
     
-    /// Invalidate the current detection task and restart if needed.
-    func invalidateURLDetection() {
-        
-        guard self.urlDetectionQueue.operationCount > 0 else { return }
-        
-        self.urlDetectionQueue.cancelAllOperations()
-        self.detectLink()
-    }
-    
-    
     /// Detect URLs in content asynchronously.
-    func detectLink() {
+    @MainActor func detectLink() {
         
         guard let textStorage = self.textStorage else { return assertionFailure() }
         guard textStorage.length > 0 else { return }
         
-        let operation = URLDetectionOperation(textStorage: textStorage)
+        let string = textStorage.string.immutable
         
-        self.urlDetectionQueue.cancelAllOperations()
-        self.urlDetectionQueue.addOperation(operation)
-    }
-    
-}
-
-
-
-final class URLDetectionOperation: AsynchronousOperation {
-     
-    // MARK: Private Properties
-    
-    private weak var textStorage: NSTextStorage?
-    private let string: String
-    
-    
-    
-    // MARK: -
-    // MARK: Lifecycle
-    
-    init(textStorage: NSTextStorage) {
-        
-        self.textStorage = textStorage
-        self.string = textStorage.string.immutable
-    }
-    
-    
-    
-    // MARK: Operation Methods
-    
-    override func main() {
-        
-        guard self.textStorage != nil else { return assertionFailure() }
-        
-        var links: [(url: URL, range: NSRange)] = []
-        let detector = try! NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
-        
-        detector.enumerateMatches(in: self.string, options: [.reportProgress], range: self.string.range) { (result, _, stop) in
-            if self.isCancelled {
-                stop.pointee = true
+        self.urlDetectionTask?.cancel()
+        self.urlDetectionTask = Task.detached(priority: .userInitiated) {
+            var links: [(url: URL, range: NSRange)] = []
+            let detector = try! NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
+            
+            detector.enumerateMatches(in: string, options: [.reportProgress], range: string.range) { (result, _, stop) in
+                if Task.isCancelled {
+                    stop.pointee = true
+                }
+                guard let result = result, let url = result.url else { return }
+                
+                links.append((url, result.range))
             }
-            guard let result = result, let url = result.url else { return }
+            try Task.checkCancellation()
             
-            links.append((url, result.range))
-        }
-        
-        DispatchQueue.main.async { [weak self] in
-            guard let self = self else { return assertionFailure() }
-            
-            guard
-                !self.isCancelled,
-                let textStorage = self.textStorage
-                else { return self.finish() }
-            
-            assert(textStorage.string.length == self.string.length, "textStorage was edited after starting URL detection")
-            
-            textStorage.beginEditing()
-            textStorage.removeAttribute(.link, range: textStorage.range)
-            for link in links {
-                textStorage.addAttribute(.link, value: link.url, range: link.range)
+            Task { @MainActor [links] in
+                assert(textStorage.string.length == string.length, "textStorage was edited after starting URL detection")
+                
+                textStorage.beginEditing()
+                textStorage.removeAttribute(.link, range: textStorage.range)
+                for link in links {
+                    textStorage.addAttribute(.link, value: link.url, range: link.range)
+                }
+                textStorage.endEditing()
             }
-            textStorage.endEditing()
-            
-            self.finish()
         }
     }
     
