0a00cb4f69475849faf32b3206d37b1014d6b41d
1024jp <1024jp@wolfrosch.com>
2022-03-14 14:16:39 +0900

    Remove unused import

--- CotEditor/Sources/AppleScript.swift
+++ CotEditor/Sources/AppleScript.swift
@@ -8,7 +8,7 @@
 //
 //  ---------------------------------------------------------------------------
 //
-//  © 2016-2020 1024jp
+//  © 2016-2022 1024jp
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -23,7 +23,6 @@
 //  limitations under the License.
 //
 
-import Combine
 import Foundation
 
 final class AppleScript: Script, AppleEventReceivable {
