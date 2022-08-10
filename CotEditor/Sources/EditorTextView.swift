6193d87a22390ef76d28031db6708c9535caca52
1024jp <1024jp@wolfrosch.com>
2022-08-10 23:45:16 +0900

    Remove workaround for selection highlight overlap

--- CotEditor/Sources/EditorTextView.swift
+++ CotEditor/Sources/EditorTextView.swift
@@ -139,12 +139,6 @@ final class EditorTextView: NSTextView, Themable, CurrentLineHighlighting, Multi
         
         super.init(coder: coder)
         
-        // workaround for: the text selection highlight can remain between lines (2017-09 macOS 10.13–10.15).
-        if !UserDefaults.standard.bool(forKey: "testsRescalingInTextView") {
-            self.scaleUnitSquare(to: NSSize(width: 0.5, height: 0.5))
-            self.scaleUnitSquare(to: self.convert(.unit, from: nil))  // reset scale
-        }
-        
         // setup layoutManager and textContainer
         let textContainer = TextContainer()
         textContainer.widthTracksTextView = true
