25688532bbd623257b119e777b6937a4985b448b
1024jp <1024jp@wolfrosch.com>
2022-08-11 01:23:18 +0900

    Fix a typo in source filename (#1342)

--- CotEditor/Sources/Document.swift
+++ CotEditor/Sources/Document.swift
@@ -66,7 +66,7 @@ final class Document: NSDocument, AdditionalDocumentPreparing, EncodingHolder {
     
     // MARK: Private Properties
     
-    private lazy var printPanelAccessoryController = PrintPanelAccessoryController.instantiate(storyboard: ProcessInfo.processInfo.operatingSystemVersion.majorVersion >= 13 ? "PrintPanelAccessoryVenture" : "PrintPanelAccessory")
+    private lazy var printPanelAccessoryController = PrintPanelAccessoryController.instantiate(storyboard: ProcessInfo.processInfo.operatingSystemVersion.majorVersion >= 13 ? "PrintPanelAccessoryVentura" : "PrintPanelAccessory")
     private var savePanelAccessoryController: NSViewController?
     
     private var readingEncoding: String.Encoding?  // encoding to read document file
