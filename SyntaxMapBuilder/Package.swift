dfe116a8c1de4e20adcf12c89306591c88ff922f
1024jp <1024jp@wolfrosch.com>
2022-10-10 13:01:09 +0900

    Update SyntaxMapBuilder version

--- SyntaxMapBuilder/Package.swift
+++ SyntaxMapBuilder/Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.7
 
 import PackageDescription
 
 let package = Package(
     name: "SyntaxMapBuilder",
     platforms: [
-        .macOS(.v11),
+        .macOS(.v12),
     ],
     products: [
         .executable(name: "SyntaxMapBuilder", targets: ["SyntaxMapBuilder"]),
