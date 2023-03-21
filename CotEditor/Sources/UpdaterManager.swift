dbe11f8cc594f311f442ec78b251b2f60a5c0cc2
1024jp <1024jp@wolfrosch.com>
2023-03-21 13:59:53 +0900

    Update copyright year

--- CotEditor/Sources/UpdaterManager.swift
+++ CotEditor/Sources/UpdaterManager.swift
@@ -8,7 +8,7 @@
 //
 //  ---------------------------------------------------------------------------
 //
-//  © 2015-2022 1024jp
+//  © 2015-2023 1024jp
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
