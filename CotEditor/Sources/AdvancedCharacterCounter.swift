aff44e45df63b54535fbb80b92f4bf4da8b091f3
1024jp <1024jp@wolfrosch.com>
2024-01-15 08:41:07 +0900

    Update copyright year

--- CotEditor/Sources/AdvancedCharacterCounter.swift
+++ CotEditor/Sources/AdvancedCharacterCounter.swift
@@ -8,7 +8,7 @@
 //
 //  ---------------------------------------------------------------------------
 //
-//  © 2022-2023 1024jp
+//  © 2022-2024 1024jp
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
