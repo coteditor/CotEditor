7da9db6e3fff83aa46b0c68bb2dbc88c8af55fe7
1024jp <1024jp@wolfrosch.com>
2024-02-12 20:25:28 +0900

    Refactor MultipleReplacePanelController

--- CotEditor/Sources/MultipleReplacePanelController.swift
+++ CotEditor/Sources/MultipleReplacePanelController.swift
@@ -8,7 +8,7 @@
 //
 //  ---------------------------------------------------------------------------
 //
-//  © 2023 1024jp
+//  © 2023-2024 1024jp
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -29,7 +29,7 @@ final class MultipleReplacePanelController: NSWindowController {
     
     // MARK: Public Properties
     
-    static let shared: MultipleReplacePanelController = MultipleReplacePanelController()
+    static let shared = MultipleReplacePanelController()
     
     
     // MARK: Lifecycle
