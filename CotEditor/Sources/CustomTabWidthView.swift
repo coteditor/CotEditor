98317604730e5343167a8fec57811af39b9de09e
1024jp <1024jp@wolfrosch.com>
2024-02-27 00:39:50 +0900

    Separate CustomTabWidth.xcstrings

--- CotEditor/Sources/CustomTabWidthView.swift
+++ CotEditor/Sources/CustomTabWidthView.swift
@@ -8,7 +8,7 @@
 //
 //  ---------------------------------------------------------------------------
 //
-//  © 2018-2023 1024jp
+//  © 2018-2024 1024jp
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -52,7 +52,7 @@ struct CustomTabWidthView: View {
     var body: some View {
         
         VStack(alignment: .trailing) {
-            LabeledContent("Tab width:") {
+            LabeledContent(String(localized: "Tab width:", table: "CustomTabWidth")) {
                 StepperNumberField(value: $value, default: self.defaultWidth, in: 1...99)
                     .onSubmit { self.submit() }
             }
