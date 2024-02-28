b2595304a7369bf782fe00169d43c47ba313b3da
1024jp <1024jp@wolfrosch.com>
2024-02-28 22:01:15 +0900

    Sanitize find history clearance

--- CotEditor/Sources/FindPanelFieldView.swift
+++ CotEditor/Sources/FindPanelFieldView.swift
@@ -8,7 +8,7 @@
 //
 //  ---------------------------------------------------------------------------
 //
-//  © 2014-2023 1024jp
+//  © 2014-2024 1024jp
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -222,7 +222,7 @@ final class FindPanelFieldViewController: NSViewController, NSTextViewDelegate {
         
         self.view.window?.makeKeyAndOrderFront(self)
         
-        UserDefaults.standard.removeObject(forKey: DefaultKeys.findHistory.rawValue)
+        UserDefaults.standard.restore(key: .findHistory)
     }
     
     
@@ -231,7 +231,7 @@ final class FindPanelFieldViewController: NSViewController, NSTextViewDelegate {
         
         self.view.window?.makeKeyAndOrderFront(self)
         
-        UserDefaults.standard.removeObject(forKey: DefaultKeys.replaceHistory.rawValue)
+        UserDefaults.standard.restore(key: .replaceHistory)
     }
     
     
