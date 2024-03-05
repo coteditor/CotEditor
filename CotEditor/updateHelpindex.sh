6aef6e1e52e4e37482d7243e1eb86c44c4581a3c
1024jp <1024jp@wolfrosch.com>
2024-03-06 00:43:38 +0900

    Udpate helpindex

--- CotEditor/updateHelpindex.sh
+++ CotEditor/updateHelpindex.sh
@@ -9,7 +9,7 @@
 #  
 #  ------------------------------------------------------------------------------
 #  
-#  © 2016-2022 1024jp
+#  © 2016-2024 1024jp
 #  
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -39,6 +39,3 @@ for dir in *.lproj; do
         echo "    ✅ ok."
     fi
 done
-
-# clear cache
-hiutil -P
