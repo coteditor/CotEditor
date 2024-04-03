17bc062e4ac10910b493abcbb255d8bcb3029bf6
1024jp <1024jp@wolfrosch.com>
2024-04-03 14:25:15 +0900

    Make ServicesProvider main actor

--- CotEditor/Sources/ServicesProvider.swift
+++ CotEditor/Sources/ServicesProvider.swift
@@ -8,7 +8,7 @@
 //
 //  ---------------------------------------------------------------------------
 //
-//  © 2015-2023 1024jp
+//  © 2015-2024 1024jp
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -25,7 +25,7 @@
 
 import AppKit
 
-final class ServicesProvider: NSObject {
+@MainActor final class ServicesProvider: NSObject {
     
     // MARK: Public Methods
     
