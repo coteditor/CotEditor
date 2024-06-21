a06eb4ecb542110e03f1e552c4e05f1b4e9d2fa9
1024jp <1024jp@wolfrosch.com>
2024-06-21 20:54:15 +0900

    Make ImageAnalysis @unchecked Sendable

--- CotEditor/Sources/LiveTextInsertionView.swift
+++ CotEditor/Sources/LiveTextInsertionView.swift
@@ -25,9 +25,10 @@
 
 import AppKit
 import SwiftUI
-@preconcurrency import VisionKit
+import VisionKit
 
 extension NSImage: @retroactive @unchecked Sendable { }
+extension ImageAnalysis: @retroactive @unchecked Sendable { }
 
 
 struct LiveTextInsertionView: View {
