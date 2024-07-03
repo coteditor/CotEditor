cd23f5bd9a2d6443aa80c8d68939511dd68b2f16
1024jp <1024jp@wolfrosch.com>
2024-07-04 01:21:51 +0900

    Add Open Document app intent

---
+++ CotEditor/Sources/DocumentShortcuts.swift
@@ -0,0 +1,71 @@
+//
+//  DocumentShortcuts.swift
+//
+//  CotEditor
+//  https://coteditor.com
+//
+//  Created by 1024jp on 2024-06-17.
+//
+//  ---------------------------------------------------------------------------
+//
+//  © 2024 1024jp
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  https://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import AppIntents
+import class AppKit.NSDocumentController
+
+struct DocumentShortcuts: AppShortcutsProvider {
+    
+    static var appShortcuts: [AppShortcut] {
+        
+        AppShortcut(intent: CreateDocumentIntent(),
+                    phrases: [
+                        "Create Document in \(.applicationName)",
+                    ],
+                    shortTitle: "Create Document",
+                    systemImageName: "text.document")
+    }
+    
+    static let shortcutTileColor: ShortcutTileColor = .lime
+}
+
+
+struct CreateDocumentIntent: AppIntent {
+    
+    static let title = LocalizedStringResource("CreateDocumentIntent.title",
+                                               defaultValue: "Create Document",
+                                               table: "Intents")
+    
+    static let description = IntentDescription(
+        .init("CreateDocumentIntent.description",
+              defaultValue: "Create a new document with the specified text in CotEditor.",
+              table: "Intents")
+    )
+    
+    static let openAppWhenRun: Bool = true
+    
+    @Parameter(title: .init("CreateDocumentIntent.Parameter.contents",
+                            defaultValue: "Contents", table: "Intents"),
+               inputOptions: .init(multiline: true))
+    var contents: String?
+    
+    
+    @MainActor func perform() async throws -> some IntentResult {
+        
+        try (DocumentController.shared as! DocumentController).openUntitledDocument(content: self.contents ?? "", display: true)
+        
+        return .result()
+    }
+}
