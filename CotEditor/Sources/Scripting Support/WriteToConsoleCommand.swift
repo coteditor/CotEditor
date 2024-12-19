51f40e31e29b0abdcda6a7ce0a676c3ee988700c
1024jp <1024jp@wolfrosch.com>
2024-12-19 11:38:07 +0900

    Update WriteToConsoleCommand.swift

--- CotEditor/Sources/Scripting Support/WriteToConsoleCommand.swift
+++ CotEditor/Sources/Scripting Support/WriteToConsoleCommand.swift
@@ -8,7 +8,7 @@
 //
 //  ---------------------------------------------------------------------------
 //
-//  © 2017-2023 1024jp
+//  © 2017-2024 1024jp
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -33,7 +33,7 @@ final class WriteToConsoleCommand: NSScriptCommand {
             let message = self.directParameter as? String,
             let arguments = self.evaluatedArguments
         else { return false }
-    
+        
         let title = (arguments["title"] as? Bool) ?? true
         let timestamp = (arguments["timestamp"] as? Bool) ?? true
         
