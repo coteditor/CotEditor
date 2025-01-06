062a3625a128a9f8944292851896e782548936cd
1024jp <1024jp@wolfrosch.com>
2025-01-06 20:57:08 +0900

    Update localization comment

--- CotEditor/Sources/Document Window/Inspector/OutlineInspectorView.swift
+++ CotEditor/Sources/Document Window/Inspector/OutlineInspectorView.swift
@@ -8,7 +8,7 @@
 //
 //  ---------------------------------------------------------------------------
 //
-//  © 2018-2024 1024jp
+//  © 2018-2025 1024jp
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -129,7 +129,7 @@ struct OutlineInspectorView: View {
             }
             .overlay {
                 if !self.filterString.isEmpty, items.isEmpty {
-                    Text("No Filter Results", tableName: "Document", comment: "display on the outline list when no results in filtering outline items")
+                    Text("No Filter Results", tableName: "Document", comment: "filtering result message")
                         .foregroundStyle(.secondary)
                         .controlSize(.regular)
                 }
