88596a9a99d0fb5c19d4fbb2baa9868e31f69d35
1024jp <1024jp@wolfrosch.com>
2025-09-06 15:25:42 +0900

    Fix unit tests in local packages

--- Packages/EditorCore/Tests/TextEditingTests/StringLineProcessingTests.swift
+++ Packages/EditorCore/Tests/TextEditingTests/StringLineProcessingTests.swift
@@ -9,7 +9,7 @@
 //
 //  ---------------------------------------------------------------------------
 //
-//  © 2020-2024 1024jp
+//  © 2020-2025 1024jp
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -26,6 +26,7 @@
 
 import Foundation
 import Testing
+import StringUtils
 @testable import TextEditing
 
 struct StringLineProcessingTests {
