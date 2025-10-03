11802a59d067ac9a8ed317d7013444f44f3fd03e
1024jp <1024jp@wolfrosch.com>
2025-10-04 01:06:14 +0900

    Toggle Swift feature flags

--- Tests/Sources/Models/DebouncerTests.swift
+++ Tests/Sources/Models/DebouncerTests.swift
@@ -8,7 +8,7 @@
 //
 //  ---------------------------------------------------------------------------
 //
-//  © 2020-2024 1024jp
+//  © 2020-2025 1024jp
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -63,9 +63,9 @@ struct DebouncerTests {
     }
     
     
-    @Test func cancel() async {
+    @MainActor @Test func cancel() async {
         
-        await confirmation("Debouncer cancelled", expectedCount: 0) { @MainActor confirm in
+        await confirmation("Debouncer cancelled", expectedCount: 0) { confirm in
             let debouncer = Debouncer {
                 confirm()
             }
