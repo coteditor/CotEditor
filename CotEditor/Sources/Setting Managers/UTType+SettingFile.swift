d321e5c2695548676d58b92e2878d1d03e31a6ea
1024jp <1024jp@wolfrosch.com>
2025-11-04 08:25:05 +0900

    Introduce “CotEditor Settings Archive” file definition (#1254)

--- CotEditor/Sources/Setting Managers/UTType+SettingFile.swift
+++ CotEditor/Sources/Setting Managers/UTType+SettingFile.swift
@@ -8,7 +8,7 @@
 //
 //  ---------------------------------------------------------------------------
 //
-//  © 2022-2023 1024jp
+//  © 2022-2025 1024jp
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -29,4 +29,5 @@ extension UTType {
     
     static let cotTheme = UTType(exportedAs: "com.coteditor.CotEditor.theme")
     static let cotReplacement = UTType(exportedAs: "com.coteditor.CotEditor.replacement")
+    static let cotSettings = UTType(exportedAs: "com.coteditor.CotEditor.settings")
 }
