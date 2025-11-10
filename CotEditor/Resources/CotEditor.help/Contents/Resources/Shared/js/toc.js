09b6f25c70a10f282145c1ed59803c1ec58dd369
1024jp <1024jp@wolfrosch.com>
2025-11-10 22:40:27 +0900

    Refactor toc.js

--- CotEditor/Resources/CotEditor.help/Contents/Resources/Shared/js/toc.js
+++ CotEditor/Resources/CotEditor.help/Contents/Resources/Shared/js/toc.js
@@ -4,21 +4,22 @@ Help Script for Table of Contents
  CotEditor
  https://coteditor.com
  
- © 2023 1024jp
+ © 2023-2025 1024jp
 */
 
-function toggleTOC() {
-    const parentDirectory = window.location.href.split('/').slice(-2)[0]
-    
-    if (parentDirectory == 'pgs') {
-        window.location = "../toc.html";
-    } else {
-        window.location = "toc.html";
-    }
-}
-
 // enable toc button in the viewer's toolbar
-window.setTimeout(function() {
-    window.HelpViewer.showTOCButton(true, toggleTOC, toggleTOC);
-    window.HelpViewer.setTOCButton(true);
-}, 100);
+if ('HelpViewer' in window) {
+    function toggleTOC() {
+        const parentDirectory = window.location.href.split('/').slice(-2)[0]
+        
+        if (parentDirectory == 'pgs') {
+            window.location = "../toc.html";
+        } else {
+            window.location = "toc.html";
+        }
+    }
+    
+    window.setTimeout(function() {
+        window.HelpViewer.showTOCButton(true, toggleTOC, toggleTOC);
+    }, 100);
+}
