67ae00946b7f80c949b0ea98ed26525d7b05837d
1024jp <1024jp@wolfrosch.com>
2026-01-31 10:56:10 +0900

    Fix unit test

--- Packages/Syntax/Tests/SyntaxTests/TreeSitterClientTests.swift
+++ Packages/Syntax/Tests/SyntaxTests/TreeSitterClientTests.swift
@@ -53,33 +53,32 @@ actor TreeSitterClientTests {
         let client = try TreeSitterClient(languageConfig: config, languageProvider: self.registry.languageProvider)
         let highlights = try await client.parseHighlights(in: source, range: source.nsRange)
         
-        #expect(highlights == [
-            ValueRange(value: .comments, range: NSRange(location: 4, length: 27)),
-            ValueRange(value: .attributes, range: NSRange(location: 36, length: 1)),
-            ValueRange(value: .types, range: NSRange(location: 37, length: 10)),
-            ValueRange(value: .attributes, range: NSRange(location: 37, length: 10)),
-            ValueRange(value: .keywords, range: NSRange(location: 48, length: 4)),
-            ValueRange(value: .commands, range: NSRange(location: 53, length: 15)),
-            ValueRange(value: .variables, range: NSRange(location: 69, length: 2)),
-            ValueRange(value: .variables, range: NSRange(location: 72, length: 6)),
-            ValueRange(value: .types, range: NSRange(location: 80, length: 6)),
-            ValueRange(value: .variables, range: NSRange(location: 88, length: 5)),
-            ValueRange(value: .types, range: NSRange(location: 95, length: 7)),
-            ValueRange(value: .keywords, range: NSRange(location: 104, length: 5)),
-            ValueRange(value: .keywords, range: NSRange(location: 110, length: 6)),
-            ValueRange(value: .types, range: NSRange(location: 121, length: 10)),
-            ValueRange(value: .variables, range: NSRange(location: 152, length: 4)),
-            ValueRange(value: .variables, range: NSRange(location: 157, length: 5)),
-            ValueRange(value: .variables, range: NSRange(location: 163, length: 14)),
-            ValueRange(value: .commands, range: NSRange(location: 163, length: 14)),
-            ValueRange(value: .keywords, range: NSRange(location: 209, length: 3)),
-            ValueRange(value: .variables, range: NSRange(location: 235, length: 21)),
-            ValueRange(value: .keywords, range: NSRange(location: 274, length: 6)),
-            ValueRange(value: .keywords, range: NSRange(location: 281, length: 3)),
-            ValueRange(value: .variables, range: NSRange(location: 285, length: 4)),
-            ValueRange(value: .variables, range: NSRange(location: 290, length: 5)),
-            ValueRange(value: .variables, range: NSRange(location: 296, length: 10)),
-            ValueRange(value: .commands, range: NSRange(location: 296, length: 10)),
-        ])
+        #expect(highlights.count == 26)
+        #expect(highlights[0] == ValueRange(value: .comments, range: NSRange(location: 4, length: 27)))
+        #expect(highlights[1] == ValueRange(value: .attributes, range: NSRange(location: 36, length: 1)))
+        #expect(highlights[2] == ValueRange(value: .types, range: NSRange(location: 37, length: 10)))
+        #expect(highlights[3] == ValueRange(value: .attributes, range: NSRange(location: 37, length: 10)))
+        #expect(highlights[4] == ValueRange(value: .keywords, range: NSRange(location: 48, length: 4)))
+        #expect(highlights[5] == ValueRange(value: .commands, range: NSRange(location: 53, length: 15)))
+        #expect(highlights[6] == ValueRange(value: .variables, range: NSRange(location: 69, length: 2)))
+        #expect(highlights[7] == ValueRange(value: .variables, range: NSRange(location: 72, length: 6)))
+        #expect(highlights[8] == ValueRange(value: .types, range: NSRange(location: 80, length: 6)))
+        #expect(highlights[9] == ValueRange(value: .variables, range: NSRange(location: 88, length: 5)))
+        #expect(highlights[10] == ValueRange(value: .types, range: NSRange(location: 95, length: 7)))
+        #expect(highlights[11] == ValueRange(value: .keywords, range: NSRange(location: 104, length: 5)))
+        #expect(highlights[12] == ValueRange(value: .keywords, range: NSRange(location: 110, length: 6)))
+        #expect(highlights[13] == ValueRange(value: .types, range: NSRange(location: 121, length: 10)))
+        #expect(highlights[14] == ValueRange(value: .keywords, range: NSRange(location: 152, length: 4)))
+        #expect(highlights[15] == ValueRange(value: .variables, range: NSRange(location: 157, length: 5)))
+        #expect(highlights[16] == ValueRange(value: .variables, range: NSRange(location: 163, length: 14)))
+        #expect(highlights[17] == ValueRange(value: .commands, range: NSRange(location: 163, length: 14)))
+        #expect(highlights[18] == ValueRange(value: .keywords, range: NSRange(location: 209, length: 3)))
+        #expect(highlights[19] == ValueRange(value: .variables, range: NSRange(location: 235, length: 21)))
+        #expect(highlights[20] == ValueRange(value: .keywords, range: NSRange(location: 274, length: 6)))
+        #expect(highlights[21] == ValueRange(value: .keywords, range: NSRange(location: 281, length: 3)))
+        #expect(highlights[22] == ValueRange(value: .keywords, range: NSRange(location: 285, length: 4)))
+        #expect(highlights[23] == ValueRange(value: .variables, range: NSRange(location: 290, length: 5)))
+        #expect(highlights[24] == ValueRange(value: .variables, range: NSRange(location: 296, length: 10)))
+        #expect(highlights[25] == ValueRange(value: .commands, range: NSRange(location: 296, length: 10)))
     }
 }
