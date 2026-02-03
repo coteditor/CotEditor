742d59d92d5ffdd6066188bb3de6c0d7c06f3862
1024jp <1024jp@wolfrosch.com>
2026-02-04 01:01:56 +0900

    Add another NSBigMutableString check

--- Tests/Sources/Extensions/NSTextStorageTests.swift
+++ Tests/Sources/Extensions/NSTextStorageTests.swift
@@ -8,7 +8,7 @@
 //
 //  ---------------------------------------------------------------------------
 //
-//  © 2024-2025 1024jp
+//  © 2024-2026 1024jp
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -37,5 +37,8 @@ struct NSTextStorageTests {
         #expect((string as NSString).className == "__NSCFString")
         #expect((bigString as NSString).className == "NSBigMutableString")
         #expect(!(bigString.immutable as NSString).className.contains("Mutable"))
+        
+        // -> Just casting to String doesn't work.
+        #expect((String(bigString) as NSString).className.contains("Mutable"))
     }
 }
