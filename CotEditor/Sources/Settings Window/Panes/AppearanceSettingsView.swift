a7dba4a21900c13435837d743a44e2006aaa9d6f
rkopicki <174372448+rkopicki@users.noreply.github.com>
2026-04-03 20:01:11 +0200

    Correct decimal point in editor settings to respect user region setting
    This fix is ​​limited to respecting user settings at the `System Settings...` > `General` > `Language & Region` > `Region` level but not at the ... > `Number format` level.

--- CotEditor/Sources/Settings Window/Panes/AppearanceSettingsView.swift
+++ CotEditor/Sources/Settings Window/Panes/AppearanceSettingsView.swift
@@ -102,7 +102,7 @@ struct AppearanceSettingsView: View {
                     .accessibilityLabeledPair(role: .label, id: "lineHeight", in: self.accessibility)
                 
                 HStack(alignment: .firstTextBaseline) {
-                    Stepper(value: $lineHeight, in: 0.1...10, step: 0.1, format: .number.precision(.fractionLength(1...2)), label: EmptyView.init)
+                    Stepper(value: $lineHeight, in: 0.1...10, step: 0.1, format: .number.precision(.fractionLength(1...2)).locale(Locale(languageCode: Locale.current.language.languageCode, languageRegion: Locale.current.region)), label: EmptyView.init)
                         .monospacedDigit()
                         .multilineTextAlignment(self.layoutDirection == .rightToLeft ? .leading : .trailing)
                         .accessibilityValue(String(localized: "\(self.lineHeight, format: .number) times", table: "AppearanceSettings",
