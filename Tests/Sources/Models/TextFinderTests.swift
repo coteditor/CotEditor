d4d2f2b934a71f72da487229609c3debe3c18424
1024jp <1024jp@wolfrosch.com>
2026-05-07 13:27:32 +0900

    Revert "Update TextFinderTests.swift"
    This reverts commit 40b343391ca681183a2987244a35ffc3fb2b3e1a.

--- Tests/Sources/Models/TextFinderTests.swift
+++ Tests/Sources/Models/TextFinderTests.swift
@@ -46,4 +46,281 @@ import Testing
         #expect(TextFinder.Action.showReplaceInterface.rawValue == NSTextFinder.Action.showReplaceInterface.rawValue)
         #expect(TextFinder.Action.hideReplaceInterface.rawValue == NSTextFinder.Action.hideReplaceInterface.rawValue)
     }
+    
+    
+    @Test func findMatchesCacheBehavior() async throws {
+        
+        do {
+            let textView = TestTextView(string: "foo foo foo")
+            textView.setSelectedRange(NSRange(0..<(textView.string as NSString).length))
+            
+            let finder = TextFinder()
+            finder.client = textView
+            finder.settings.findString = "foo"
+            finder.settings.usesRegularExpression = false
+            
+            _ = try await self.performFindAction(.nextMatch, with: finder)
+            let firstCache = try #require(finder.findMatchesCache)
+            
+            textView.setSelectedRange(NSRange(0..<(textView.string as NSString).length))
+            _ = try await self.performFindAction(.nextMatch, with: finder)
+            let secondCache = try #require(finder.findMatchesCache)
+            
+            #expect(firstCache.options.textVersion == secondCache.options.textVersion)
+            #expect(firstCache.matches == secondCache.matches)
+        }
+        
+        do {
+            let textView = TestTextView(string: "foo bar foo")
+            textView.setSelectedRange(NSRange(0..<(textView.string as NSString).length))
+            
+            let finder = TextFinder()
+            finder.client = textView
+            finder.settings.findString = "foo"
+            finder.settings.usesRegularExpression = false
+            
+            let firstResult = try await self.performFindAction(.nextMatch, with: finder)
+            let firstCache = try #require(finder.findMatchesCache)
+            
+            textView.textStorage?.replaceCharacters(in: NSRange(0..<3), with: "xxx")
+            textView.setSelectedRange(NSRange(0..<(textView.string as NSString).length))
+            
+            let secondResult = try await self.performFindAction(.nextMatch, with: finder)
+            let secondCache = try #require(finder.findMatchesCache)
+            
+            #expect(firstResult.count == 2)
+            #expect(secondResult.count == 1)
+            #expect(firstCache.options.textVersion != secondCache.options.textVersion)
+        }
+        
+        do {
+            let textView = TestTextView(string: "foo bar foo")
+            textView.setSelectedRange(NSRange(0..<(textView.string as NSString).length))
+            
+            let finder = TextFinder()
+            finder.client = textView
+            finder.settings.findString = "foo"
+            finder.settings.usesRegularExpression = false
+            
+            _ = try await self.performFindAction(.nextMatch, with: finder)
+            let firstCache = try #require(finder.findMatchesCache)
+            
+            finder.settings.findString = "bar"
+            textView.setSelectedRange(NSRange(0..<(textView.string as NSString).length))
+            
+            let result = try await self.performFindAction(.nextMatch, with: finder)
+            let secondCache = try #require(finder.findMatchesCache)
+            
+            #expect(result.count == 1)
+            #expect(textView.selectedRange() == NSRange(4..<7))
+            #expect(firstCache.options.textVersion == secondCache.options.textVersion)
+            #expect(firstCache.matches != secondCache.matches)
+        }
+        
+        do {
+            let textView = TestTextView(string: "foo bar foo")
+            
+            let finder = TextFinder()
+            finder.client = textView
+            finder.settings.findString = "foo"
+            finder.settings.usesRegularExpression = false
+            
+            textView.setSelectedRange(NSRange(0..<3))
+            _ = try await self.performFindAction(.nextMatch, with: finder)
+            let firstCache = try #require(finder.findMatchesCache)
+            
+            textView.setSelectedRange(NSRange(8..<11))
+            let result = try await self.performFindAction(.nextMatch, with: finder)
+            let secondCache = try #require(finder.findMatchesCache)
+            
+            if finder.settings.inSelection {
+                #expect(result.count == 1)
+                #expect(textView.selectedRange() == NSRange(8..<11))
+                #expect(firstCache.matches != secondCache.matches)
+            } else {
+                #expect(result.count == 2)
+                #expect(firstCache.matches == secondCache.matches)
+            }
+        }
+    }
+    
+    
+    @Test func findResultMatchMetadata() async throws {
+        
+        do {
+            let textView = TestTextView(string: "foo bar foo")
+            
+            let finder = TextFinder()
+            finder.client = textView
+            finder.settings.findString = "foo"
+            finder.settings.usesRegularExpression = false
+            
+            if finder.settings.inSelection {
+                textView.setSelectedRange(NSRange(0..<(textView.string as NSString).length))
+            } else {
+                textView.setSelectedRange(NSRange(0..<0))
+            }
+            
+            let firstResult = try await self.performFindAction(.nextMatch, with: finder)
+            let secondResult = try await self.performFindAction(.nextMatch, with: finder)
+            
+            #expect(firstResult.count == 2)
+            #expect(firstResult.currentMatchIndex == 1)
+            #expect(firstResult.matchedRange == NSRange(0..<3))
+            
+            if finder.settings.inSelection {
+                #expect(secondResult.currentMatchIndex == 1)
+                #expect(secondResult.matchedRange == NSRange(0..<3))
+            } else {
+                #expect(secondResult.currentMatchIndex == 2)
+                #expect(secondResult.matchedRange == NSRange(8..<11))
+                
+                let previousResult = try await self.performFindAction(.previousMatch, with: finder)
+                #expect(previousResult.currentMatchIndex == 1)
+                #expect(previousResult.matchedRange == NSRange(0..<3))
+            }
+        }
+        
+        do {
+            let textView = TestTextView(string: "bar baz")
+            textView.setSelectedRange(NSRange(0..<(textView.string as NSString).length))
+            
+            let finder = TextFinder()
+            finder.client = textView
+            finder.settings.findString = "foo"
+            finder.settings.usesRegularExpression = false
+            
+            let result = try await self.performFindAction(.nextMatch, with: finder)
+            
+            #expect(result.count == 0)
+            #expect(result.currentMatchIndex == nil)
+            #expect(result.matchedRange == nil)
+        }
+    }
+    
+    
+    @Test(arguments: [TextFinder.Action.findAll, .highlight])
+    func findAllPreservesReadOnlyState(action: TextFinder.Action) async throws {
+        
+        let textView = TestTextView(string: "foo bar foo")
+        textView.setSelectedRange(NSRange(0..<(textView.string as NSString).length))
+        textView.isEditable = false
+        
+        let finder = TextFinder()
+        finder.client = textView
+        finder.settings.findString = "foo"
+        finder.settings.usesRegularExpression = false
+        
+        _ = try await self.performFindAction(action, with: finder)
+        
+        #expect(!textView.isEditable)
+    }
+    
+    
+    @Test func selectAllUsesFindMatchesCache() async throws {
+        
+        do {
+            let textView = TestTextView(string: "foo bar foo")
+            textView.setSelectedRange(NSRange(0..<(textView.string as NSString).length))
+            
+            let finder = TextFinder()
+            finder.client = textView
+            finder.settings.findString = "foo"
+            finder.settings.usesRegularExpression = false
+            
+            let result = try await self.performFindAction(.selectAll, with: finder)
+            let selectedRanges = textView.selectedRanges.map(\.rangeValue)
+            let cache = try #require(finder.findMatchesCache)
+            
+            #expect(result.count == 2)
+            #expect(selectedRanges == [NSRange(0..<3), NSRange(8..<11)])
+            #expect(cache.matches == selectedRanges)
+        }
+        
+        do {
+            let textView = TestTextView(string: "foo bar foo")
+            textView.setSelectedRange(NSRange(0..<(textView.string as NSString).length))
+            
+            let finder = TextFinder()
+            finder.client = textView
+            finder.settings.findString = "foo"
+            finder.settings.usesRegularExpression = false
+            
+            try await confirmation("No stale find result", expectedCount: 0) { confirm in
+                let notifications = NotificationCenter.default.notifications(named: TextFinder.DidFindMessage.name, object: finder)
+                let observationTask = Task { @MainActor in
+                    for await _ in notifications {
+                        confirm()
+                    }
+                }
+                
+                finder.performAction(.selectAll)
+                textView.textStorage?.replaceCharacters(in: NSRange(0..<3), with: "bar")
+                
+                try await Task.sleep(for: .milliseconds(50))
+                observationTask.cancel()
+            }
+        }
+    }
+    
+    
+    // MARK: Private Methods
+    
+    private func performFindAction(_ action: TextFinder.Action, with finder: TextFinder) async throws -> FindResult {
+        
+        let notifications = NotificationCenter.default.notifications(named: TextFinder.DidFindMessage.name, object: finder)
+        let iterator = notifications.makeAsyncIterator()
+        
+        finder.performAction(action)
+        
+        let notification = try #require(await iterator.next())
+        return try #require(notification.userInfo?["result"] as? FindResult)
+    }
+}
+
+
+// MARK: -
+
+@MainActor private final class TestTextView: NSTextView {
+    
+    init(string: String) {
+        
+        let textStorage = NSTextStorage(string: string)
+        let layoutManager = TestLayoutManager()
+        let textContainer = NSTextContainer()
+        
+        layoutManager.addTextContainer(textContainer)
+        textStorage.addLayoutManager(layoutManager)
+        
+        super.init(frame: .zero, textContainer: textContainer)
+        
+        self.isEditable = true
+        self.isSelectable = true
+        self.string = string
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    override func showFindIndicator(for charRange: NSRange) { }
+    
+    
+    var hasTemporaryBackgroundColor: Bool {
+        
+        guard let layoutManager else { return false }
+        
+        return (0..<(self.string as NSString).length).contains {
+            layoutManager.temporaryAttribute(.backgroundColor, atCharacterIndex: $0, effectiveRange: nil) != nil
+        }
+    }
+}
+
+
+private final class TestLayoutManager: NSLayoutManager, ValidationIgnorable {
+    
+    var ignoresDisplayValidation = false
 }
