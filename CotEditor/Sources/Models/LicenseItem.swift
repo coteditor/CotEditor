ac9c7775dc2d523450928f14fa67be2ec03856ce
1024jp <1024jp@wolfrosch.com>
2026-08-27 08:36:14 +0900

    Adjust indent in License.swift

--- CotEditor/Sources/Models/LicenseItem.swift
+++ CotEditor/Sources/Models/LicenseItem.swift
@@ -31,7 +31,7 @@ struct LicenseItem {
     var url: String
     var copyright: String
     var license: License
-    var description: String?
+    var description: LocalizedStringResource?
     
     
 #if SPARKLE
@@ -154,18 +154,18 @@ struct LicenseItem {
         name: "Sparkle",
         url: "https://sparkle-project.org",
         copyright: """
-                                    © 2006-2013 Andy Matuschak.
-                                    © 2009-2013 Elgato Systems GmbH.
-                                    © 2011-2014 Kornel Lesiński.
-                                    © 2015-2017 Mayur Pawashe.
-                                    © 2014 C.W. Betts.
-                                    © 2014 Petroules Corporation.
-                                    © 2014 Big Nerd Ranch.
-                                    All rights reserved.
-                                    """,
+                   © 2006-2013 Andy Matuschak.
+                   © 2009-2013 Elgato Systems GmbH.
+                   © 2011-2014 Kornel Lesiński.
+                   © 2015-2017 Mayur Pawashe.
+                   © 2014 C.W. Betts.
+                   © 2014 Petroules Corporation.
+                   © 2014 Big Nerd Ranch.
+                   All rights reserved.
+                   """,
         license: .custom("Sparkle"),
-        description: String(localized: "only on non-AppStore version", table: "About",
-                            comment: "annotation for the Sparkle framework license")
+        description: .init("only on non-AppStore version", table: "About",
+                           comment: "annotation for the Sparkle framework license")
     )
 }
 
