0e1a702bbe19b6b3b8fa2001a63194dfdbc8fad6
1024jp <1024jp@wolfrosch.com>
2026-09-24 04:45:30 +0900

    Adjust syntax editor’s sidebar width

--- CotEditor/Sources/Settings Window/Syntax Editor/SyntaxEditView.swift
+++ CotEditor/Sources/Settings Window/Syntax Editor/SyntaxEditView.swift
@@ -139,7 +139,7 @@ struct SyntaxEditView: View {
             .focused($focus, equals: .sidebar)
             .simultaneousGesture(TapGesture().onEnded { self.focus = .sidebar })  // workaround (2026-08, macOS 27)
             .environment(\.sidebarRowSize, .medium)
-            .navigationSplitViewColumnWidth(min: 80, ideal: 160)
+            .navigationSplitViewColumnWidth(min: 80, ideal: 180)
             
         } detail: {
             VStack(spacing: 0) {
